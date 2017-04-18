88249b0c2190a174626fc105c965109d8c122078
Jan Funke <jfunke@iri.upc.edu>
2017-04-18 12:32:28 -0400

    Provide setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='gunpowder',
+        version='0.1',
+        description='Data loading DAG for Greentea.',
+        url='https://github.com/funkey/gunpowder',
+        author='Jan Funke',
+        author_email='jfunke@iri.upc.edu',
+        license='MIT',
+        packages=['gunpowder'],
+)
