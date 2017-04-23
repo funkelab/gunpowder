7bfcd34a5c901a76138948a19cb2a016e2556530
Jan Funke <jfunke@iri.upc.edu>
2017-04-23 09:44:25 -0400

    ProducerPool: Ensure watch dog cleans up on exit

--- gunpowder/producer_pool.py
+++ gunpowder/producer_pool.py
@@ -16,7 +16,7 @@ class WorkersDied(Exception):
 class ProducerPool(object):
 
     def __init__(self, callables, queue_size=10):
-        self.watch_dog = multiprocessing.Process(target=self.__run_watch_dog, args=(callables,))
+        self.__watch_dog = multiprocessing.Process(target=self.__run_watch_dog, args=(callables,))
         self.__stop = multiprocessing.Event()
         self.__result_queue = multiprocessing.Queue(queue_size)
 
@@ -26,12 +26,12 @@ class ProducerPool(object):
     def start(self):
         '''Start the pool of producers.'''
 
-        if self.watch_dog.is_alive():
+        if self.__watch_dog.is_alive():
             logger.warning("trying to start workers, but they are already running")
             return
 
         self.__stop.clear()
-        self.watch_dog.start()
+        self.__watch_dog.start()
 
     def get(self, timeout=0):
         '''Return the next result from the producer pool.
@@ -54,7 +54,6 @@ class ProducerPool(object):
             try:
                 item = self.__result_queue.get(timeout=timeout)
             except Queue.Empty:
-                # logger.debug("queue is still empty")
                 if not block:
                     raise NoResult()
 
@@ -68,12 +67,12 @@ class ProducerPool(object):
         Items currently being produced will not be waited for and be discarded.'''
 
         self.__stop.set()
-        self.watch_dog.join()
+        self.__watch_dog.join()
 
     def alive(self):
         '''Test if the pool is alive (i.e., all workers are running).
         '''
-        return self.watch_dog.is_alive()
+        return self.__watch_dog.is_alive()
 
     def __run_watch_dog(self, callables):
 
@@ -84,27 +83,33 @@ class ProducerPool(object):
 
         workers = [ multiprocessing.Process(target=self.__run_worker, args=(c,)) for c in callables ]
 
-        logger.debug("starting %d workers"%len(workers))
-        for worker in workers:
-            worker.start()
+        try:
 
-        while not self.__stop.wait(1):
-            if os.getppid() != parent_pid:
-                logger.error("parent of producer pool died, shutting down")
-                break
-            if not self.__all_workers_alive(workers):
-                logger.error("at least one of my workers died, shutting down")
-                break
+            logger.debug("starting %d workers"%len(workers))
+            for worker in workers:
+                worker.start()
 
-        logger.info("terminating workers...")
-        for worker in workers:
-            worker.terminate()
+            while not self.__stop.wait(1):
+                if os.getppid() != parent_pid:
+                    logger.error("parent of producer pool died, shutting down")
+                    break
+                if not self.__all_workers_alive(workers):
+                    logger.error("at least one of my workers died, shutting down")
+                    break
+        except:
+            pass
 
-        logger.info("joining workers...")
-        for worker in workers:
-            worker.join()
+        finally:
 
-        logger.info("done")
+            logger.info("terminating workers...")
+            for worker in workers:
+                worker.terminate()
+
+            logger.info("joining workers...")
+            for worker in workers:
+                worker.join()
+
+            logger.info("done")
 
     def __run_worker(self, target):
 
@@ -113,32 +118,34 @@ class ProducerPool(object):
         logger.debug("worker started with PID " + str(os.getpid()))
         logger.debug("parent PID " + str(parent_pid))
 
-        stop = False
-        while not stop:
+        result = None
+        while True:
 
-            result = None
+            if os.getppid() != parent_pid:
+                logger.debug("worker %d: watch-dog died, stopping"%os.getpid())
+                break
 
-            try:
-                result = target()
-            except Exception as e:
-                result = e
-                stop = True
-            except:
-                logger.error(sys.exc_info()[0])
-                stop = True
-
-            while result is not None:
-
-                if os.getppid() != parent_pid:
-                    logger.debug("worker %d: watch-dog died, stopping"%os.getpid())
-                    stop = True
-                    break
+            if result is None:
 
                 try:
-                    self.__result_queue.put(result, timeout=1)
-                    result = None
-                except Queue.Full:
-                    logger.debug("worker %d: result queue is full, waiting to place my result"%os.getpid())
+                    result = target()
+                except Exception as e:
+                    result = e
+                    # don't stop on normal exceptions -- place them in result queue 
+                    # and let them be handled by caller
+                except:
+                    logger.error("received error: " + str(sys.exc_info()[0]))
+                    # this is most likely a keyboard interrupt, stop process
+                    break
+
+            try:
+                self.__result_queue.put(result, timeout=1)
+                result = None
+            except Queue.Full:
+                logger.debug("worker %d: result queue is full, waiting to place my result"%os.getpid())
+
+        logger.debug("worker with PID " + str(os.getpid()) + " exiting")
+        os._exit(1)
 
     def __all_workers_alive(self, workers):
         return all([ worker.is_alive() for worker in workers ])
