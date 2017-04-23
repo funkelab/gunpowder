6b32fe54ebe2fcb7da91039c81963fb902e6c387
Jan Funke <jfunke@iri.upc.edu>
2017-04-23 09:43:51 -0400

    Add debug output to gunpowder.build

--- gunpowder/build.py
+++ gunpowder/build.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 class build(object):
 
     def __init__(self, batch_provider):
@@ -7,9 +10,13 @@ class build(object):
         try:
             self.batch_provider.setup()
         except:
+            logger.error("something went wrong during the setup of the pipeline, calling tear down")
             self.batch_provider.teardown()
+            logger.debug("tear down completed")
             raise
         return self.batch_provider
 
     def __exit__(self, type, value, traceback):
+        logger.debug("leaving context, tearing down pipeline")
         self.batch_provider.teardown()
+        logger.debug("tear down completed")
