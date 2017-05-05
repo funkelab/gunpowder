2f0009eaccc636d2697356398d45d182b28c618c
Jan Funke <jfunke@iri.upc.edu>
2017-05-05 07:42:20 -0400

    Simplify examples/fib25/train.sh
    This example should be self-containted, i.e., should not assume the
    existence of certain users or directories.

--- examples/fib25/train.sh
+++ examples/fib25/train.sh
@@ -1,32 +1,22 @@
+#!/usr/bin/env bash
+
 rm snapshots/*
 rm net_iter*
 
 NAME=$(basename "$PWD")
 
-# docker build \
-#     /groups/turaga/home/grisaitisw/src/gunpowder/docker/grisaitis_mod \
-#     -t grisaitisw/gunpowder:latest
+sudo nvidia-docker rm -f $NAME
 
-nvidia-docker rm -f $NAME
+USER_ID=${UID}
+USER_HOME=${HOME}
 
-WILLIAM_USER_ID=53850
-WILLIAM_HOME=/groups/turaga/home/grisaitisw
+echo "Starting as user ${USER_ID} with home ${HOME}"
 
-NV_GPU=1 nvidia-docker run --rm \
-    -u $WILLIAM_USER_ID \
-    -e "HOME=$WILLIAM_HOME" \
-    -v /groups/turaga/home:/groups/turaga/home \
-    -v $WILLIAM_HOME/src/gunpowder:/src/gunpowder \
-    -w $(pwd) \
-    --name $NAME \
-    grisaitisw/gunpowder:latest \
-    /bin/bash -c "PYTHONPATH=$WILLIAM_HOME/src/dvision:\$PYTHONPATH && python -u train.py"
-
-#    /bin/bash -c "PYTHONPATH=~/src/dvision:\$PYTHONPATH && python -c 'import sys; from pprint import pprint; pprint(sys.path)'"
-#    /bin/bash -c "PYTHONPATH=~/src/gunpowder:~/src/dvision:\$PYTHONPATH && python -c 'import gunpowder; print gunpowder.__file__'"
-#    /bin/bash -c "PYTHONPATH=~/src/gunpowder:~/src/dvision:\$PYTHONPATH && python -u train.py"
-#    /bin/bash -c "PYTHONPATH=~/src/gunpowder:\$PYTHONPATH && env && ls -l ~ && pwd && ls -l"
-
-#todo: fix docker image
-#error:
-#ImportError: /opt/conda/lib/libstdc++.so.6: version `GLIBCXX_3.4.21' not found (required by /opt/caffe/python/caffe/_caffe.so)
+NV_GPU=1 sudo nvidia-docker run --rm \
+    -u ${USER_ID} \
+    -e HOME=${USER_HOME} \
+    -v ${PWD}:/run \
+    -w /run \
+    --name ${NAME} \
+    funkey/gunpowder:latest \
+    python -u train.py
