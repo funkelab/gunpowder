4c9211f9ef252d29563f4d204bdcaf057b2dbb8a
Jan Funke <jfunke@iri.upc.edu>
2017-05-11 06:57:07 -0400

    Make Snapshot more verbose

--- gunpowder/snapshot.py
+++ gunpowder/snapshot.py
@@ -41,7 +41,7 @@ class Snapshot(BatchFilter):
                 pass
 
             snapshot_name = os.path.join(self.output_dir, self.output_filename.format(id=str(id).zfill(8)))
-            logger.debug("saving to " + snapshot_name)
+            logger.info("saving to " + snapshot_name)
             with h5py.File(snapshot_name, 'w') as f:
                 f['volumes/raw'] = batch.raw
                 f['volumes/raw'].attrs['offset'] = batch.spec.input_roi.get_offset()
