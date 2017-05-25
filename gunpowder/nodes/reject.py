4cb85e04ebea7d3eb98569f142643d2b20ad0f9c
Jan Funke <jfunke@iri.upc.edu>
2017-05-25 15:40:55 -0400

    Make Reject a BatchProvider (not BatchFilter)

--- gunpowder/nodes/reject.py
+++ gunpowder/nodes/reject.py
@@ -1,19 +1,24 @@
-import copy
 import logging
 
-from .batch_filter import BatchFilter
+from .batch_provider import BatchProvider
 from gunpowder.profiling import Timing
 from gunpowder.volume import VolumeType
 
 logger = logging.getLogger(__name__)
 
-class Reject(BatchFilter):
+class Reject(BatchProvider):
 
     def __init__(self, min_masked=0.5):
         self.min_masked = min_masked
 
     def setup(self):
         assert VolumeType.GT_MASK in self.get_spec().volumes, "Reject can only be used if GT masks are provided"
+        assert len(self.get_upstream_providers()) == 1, "Reject can only be used with exactly one upstream provider."
+        self.upstream_provider = self.get_upstream_providers()[0]
+
+    def get_spec(self):
+        assert len(self.get_upstream_providers()) == 1, "Reject can only be used with exactly one upstream provider."
+        return self.get_upstream_providers()[0].get_spec()
 
     def provide(self, request):
 
@@ -28,7 +33,7 @@ class Reject(BatchFilter):
         have_good_batch = False
         while not have_good_batch:
 
-            batch = self.get_upstream_provider().request_batch(copy.copy(request))
+            batch = self.upstream_provider.request_batch(request)
             mask_ratio = batch.volumes[VolumeType.GT_MASK].data.mean()
             have_good_batch = mask_ratio>=self.min_masked
 
