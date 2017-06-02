44aa4930355d9c8831e5c893c6d6cee807a04a0d
Jan Funke <jfunke@iri.upc.edu>
2017-06-02 10:04:13 -0400

    Allow users to specify mask type for Reject

--- gunpowder/nodes/reject.py
+++ gunpowder/nodes/reject.py
@@ -8,11 +8,12 @@ logger = logging.getLogger(__name__)
 
 class Reject(BatchProvider):
 
-    def __init__(self, min_masked=0.5):
+    def __init__(self, min_masked=0.5, mask_volume_type=VolumeType.GT_MASK):
         self.min_masked = min_masked
+        self.mask_volume_type = mask_volume_type
 
     def setup(self):
-        assert VolumeType.GT_MASK in self.get_spec().volumes, "Reject can only be used if GT masks are provided"
+        assert self.mask_volume_type in self.get_spec().volumes, "Reject can only be used if %s is provided"%mask_volume_type
         assert len(self.get_upstream_providers()) == 1, "Reject can only be used with exactly one upstream provider."
         self.upstream_provider = self.get_upstream_providers()[0]
 
@@ -28,18 +29,18 @@ class Reject(BatchProvider):
         timing = Timing(self)
         timing.start()
 
-        assert VolumeType.GT_MASK in request.volumes, "Reject can only be used if a GT mask is requested"
+        assert self.mask_volume_type in request.volumes, "Reject can only be used if a GT mask is requested"
 
         have_good_batch = False
         while not have_good_batch:
 
             batch = self.upstream_provider.request_batch(request)
-            mask_ratio = batch.volumes[VolumeType.GT_MASK].data.mean()
+            mask_ratio = batch.volumes[self.mask_volume_type].data.mean()
             have_good_batch = mask_ratio>=self.min_masked
 
             if not have_good_batch:
 
-                logger.debug("reject batch with mask ratio %f at "%mask_ratio + str(batch.volumes[VolumeType.GT_MASK].roi))
+                logger.debug("reject batch with mask ratio %f at "%mask_ratio + str(batch.volumes[self.mask_volume_type].roi))
                 num_rejected += 1
 
                 if timing.elapsed() > report_next_timeout:
@@ -47,7 +48,7 @@ class Reject(BatchProvider):
                     logger.warning("rejected %d batches, been waiting for a good one since %ds"%(num_rejected, report_next_timeout))
                     report_next_timeout *= 2
 
-        logger.debug("good batch with mask ratio %f found at "%mask_ratio + str(batch.volumes[VolumeType.GT_MASK].roi))
+        logger.debug("good batch with mask ratio %f found at "%mask_ratio + str(batch.volumes[self.mask_volume_type].roi))
 
         timing.stop()
         batch.profiling_stats.add(timing)
