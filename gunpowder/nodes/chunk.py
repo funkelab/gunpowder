8969d11018ed454f2b017a90a2a3ece1510e3806
rekrau <renate.b.krause@bluewin.ch>
2017-07-06 08:55:33 -0400

    Extend  chunk note to work with batch.points

--- gunpowder/nodes/chunk.py
+++ gunpowder/nodes/chunk.py
@@ -7,6 +7,7 @@ from .batch_filter import BatchFilter
 from gunpowder.batch import Batch
 from gunpowder.coordinate import Coordinate
 from gunpowder.producer_pool import ProducerPool
+from gunpowder.points import PointsType, PointsOfType
 from gunpowder.volume import VolumeTypes, Volume
 
 logger = logging.getLogger(__name__)
@@ -20,9 +21,10 @@ class Chunk(BatchFilter):
         self.chunk_spec_template = chunk_spec
         self.dims = self.chunk_spec_template.volumes[self.chunk_spec_template.volumes.keys()[0]].dims()
 
-        for volume_type in self.chunk_spec_template.volumes:
-            assert self.dims == self.chunk_spec_template.volumes[volume_type].dims(),\
-                "Volumes of different dimensionalities cannot be handled by chunk"
+        for collection_type in [self.chunk_spec_template.volumes, self.chunk_spec_template.points]:
+            for type in collection_type:
+                assert self.dims == collection_type[type].dims(),\
+                    "Rois of different dimensionalities cannot be handled by chunk"
 
         self.request = copy.deepcopy(request)
         self.num_workers = num_workers
@@ -58,6 +60,10 @@ class Chunk(BatchFilter):
                 self.__fill(batch.volumes[volume_type].data, volume.data,
                             request.volumes[volume_type], volume.roi)
 
+            for (points_type, points_of_type) in chunk.points.items():
+                self.__fill_points(batch.points[points_type].data, points_of_type.data,
+                                   request.points[points_type], points_of_type.roi)
+
         return batch
 
     def __setup_batch(self, request, chunk_batch):
@@ -66,12 +72,21 @@ class Chunk(BatchFilter):
         for (volume_type, roi) in request.volumes.items():
             if volume_type == VolumeTypes.PRED_AFFINITIES or volume_type == VolumeTypes.GT_AFFINITIES:
                 shape = (3,)+ roi.get_shape()
+            elif volume_type == VolumeTypes.PRED_BM_PRESYN:
+                shape = (1,)+roi.get_shape()
             else:
                 shape = roi.get_shape()
 
             batch.volumes[volume_type] = Volume(data=np.zeros(shape),
                                                 roi=roi,
                                                 resolution=chunk_batch.volumes[VolumeTypes.RAW].resolution)
+
+        for (points_type, roi) in request.points.items():
+            batch.points[points_type] = PointsOfType(data={},
+                                                        roi=roi,
+                                                        resolution=chunk_batch.volumes[VolumeTypes.RAW].resolution)
+
+
         return batch
 
     def __fill(self, a, b, roi_a, roi_b):
@@ -93,6 +108,23 @@ class Chunk(BatchFilter):
 
         a[slices_a] = b[slices_b]
 
+    def __fill_points(self, a, b, roi_a, roi_b):
+        logger.debug("filling points of " + str(roi_b) + " into points of" + str(roi_a))
+
+        common_roi = roi_a.intersect(roi_b)
+        if common_roi is None:
+            return
+
+        # find max point_id in a so far
+        max_point_id = 0
+        for point_id, point in a.items():
+            if point_id > max_point_id:
+                max_point_id = point_id
+
+        for point_id, point in b.items():
+            if roi_a.contains(Coordinate(point.location)):
+                a[point_id + max_point_id] = point
+
     def __run_worker(self, i):
         chosen_request = self.requests.get()
         return self.get_upstream_provider().request_batch(chosen_request)
@@ -108,14 +140,18 @@ class Chunk(BatchFilter):
 
         # initial shift required per volume to be at beginning of its requested roi
         all_initial_offsets = []
-        for (volume_type, roi) in self.chunk_spec_template.volumes.items():
-            all_initial_offsets.append(self.request.volumes[volume_type].get_begin() - roi.get_begin())
+        for chunk_spec_type, request_type in zip([self.chunk_spec_template.volumes, self.chunk_spec_template.points],
+                                                 [self.request.volumes, self.request.points]):
+            for (type, roi) in chunk_spec_type.items():
+                all_initial_offsets.append(request_type[type].get_begin() - roi.get_begin())
         begin = np.min(all_initial_offsets, axis=0)
 
         # max offsets required per volume to cover their entire requested roi
         all_max_offsets = []
-        for (volume_type, roi) in self.chunk_spec_template.volumes.items():
-            all_max_offsets.append(self.request.volumes[volume_type].get_end()-self.chunk_spec_template.volumes[volume_type].get_shape())
+        for chunk_spec_type, request_type in zip([self.chunk_spec_template.volumes, self.chunk_spec_template.points],
+                                                 [self.request.volumes, self.request.points]):
+            for (type, roi) in chunk_spec_type.items():
+                all_max_offsets.append(request_type[type].get_end()-chunk_spec_type[type].get_shape())
         end = np.max(all_max_offsets, axis=0) + min_stride
 
         offset = np.array(begin)
@@ -125,19 +161,22 @@ class Chunk(BatchFilter):
             chunk_request = copy.deepcopy(self.request)
             max_strides = []
             # change size and offset of the batch spec
-            for volume_type, roi in self.chunk_spec_template.volumes.items():
-                chunk_request.volumes[volume_type] = roi + Coordinate(offset)
-                # adjust stride to be as large as possible. Chunk roi lies either:
-                #   in front and within roi, then max stride shifts chunk roi to begin of request roi
-                #   behind requested roi, ten max stride shifts chunk roi to end of ALL rois in request
-                # finally, clip max_stride s.t. it is not smaller than min_stride
-                max_stride = np.zeros([3])
-                for dim in range(roi.dims()):
-                    if self.request.volumes[volume_type].get_end()[dim] > chunk_request.volumes[volume_type].get_end()[dim]:
-                        max_stride[dim] = self.request.volumes[volume_type].get_begin()[dim] - chunk_request.volumes[volume_type].get_begin()[dim]
-                    else:
-                        max_stride[dim] = end[dim] - offset[dim]
-                max_strides.append(max_stride.clip(min_stride))
+            for chunk_spec_type, chunk_request_type, request_type in zip([self.chunk_spec_template.volumes, self.chunk_spec_template.points],
+                                                                            [chunk_request.volumes, chunk_request.points],
+                                                                            [self.request.volumes, self.request.points]):
+                for type, roi in chunk_spec_type.items():
+                    chunk_request_type[type] = roi + Coordinate(offset)
+                    # adjust stride to be as large as possible. Chunk roi lies either:
+                    #   in front and within roi, then max stride shifts chunk roi to begin of request roi
+                    #   behind requested roi, ten max stride shifts chunk roi to end of ALL rois in request
+                    # finally, clip max_stride s.t. it is not smaller than min_stride
+                    max_stride = np.zeros([3])
+                    for dim in range(roi.dims()):
+                        if request_type[type].get_end()[dim] > chunk_request_type[type].get_end()[dim]:
+                            max_stride[dim] = request_type[type].get_begin()[dim] - chunk_request_type[type].get_begin()[dim]
+                        else:
+                            max_stride[dim] = end[dim] - offset[dim]
+                    max_strides.append(max_stride.clip(min_stride))
 
             stride = np.min(max_strides, axis=0)
 
