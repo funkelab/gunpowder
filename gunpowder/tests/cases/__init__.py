d1da4a9cee900833a8c7cdd97d4b544e0a1f783b
rekrau <renate.b.krause@bluewin.ch>
2017-07-06 10:31:56 -0400

    Add TestPointsTypes to unittest test_suite

--- gunpowder/tests/cases/__init__.py
+++ gunpowder/tests/cases/__init__.py
@@ -2,3 +2,4 @@ from .provider_test import ProviderTest
 from .normalize import TestNormalize
 from .chunk import TestChunk
 from .volume_types import TestVolumeTypes
+from .points_types import TestPointsTypes
