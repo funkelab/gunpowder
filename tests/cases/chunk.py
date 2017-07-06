1f16715e8cf4d0250b5aa9f4b71142ab70d9917f
rekrau <renate.b.krause@bluewin.ch>
2017-07-06 14:51:58 -0400

    Update unittest chunk.py for chunk node working with parallel requests

--- tests/cases/chunk.py
+++ tests/cases/chunk.py
@@ -56,7 +56,7 @@ class TestChunk(ProviderTest):
             }
         )
 
-        pipeline = ChunkTestSource() + Chunk(chunk_request)
+        pipeline = ChunkTestSource() + Chunk(full_request, chunk_request, num_workers=1)
 
         with build(pipeline):
             batch = pipeline.request_batch(full_request)
