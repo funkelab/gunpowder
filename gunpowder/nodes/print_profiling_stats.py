87358c4508bc133cb12a21a60173794f146364f7
Jan Funke <jfunke@iri.upc.edu>
2017-07-07 10:28:41 -0400

    Profile upstream and downstream passes in PrintProfilingStats

--- gunpowder/nodes/print_profiling_stats.py
+++ gunpowder/nodes/print_profiling_stats.py
@@ -1,7 +1,7 @@
 import logging
 
 from .batch_filter import BatchFilter
-from gunpowder.profiling import ProfilingStats
+from gunpowder.profiling import Timing, ProfilingStats
 
 logger = logging.getLogger(__name__)
 
@@ -12,9 +12,16 @@ class PrintProfilingStats(BatchFilter):
         self.every = every
         self.n = 0
         self.accumulated_stats = ProfilingStats()
-        self.__last_span_end = 0
+        self.__upstream_timing = Timing(self)
+        self.__downstream_timing = Timing(self)
+
+    def prepare(self, request):
+        self.__upstream_timing.start()
+        self.__downstream_timing.stop()
 
     def process(self, batch, request):
+        self.__upstream_timing.stop()
+        self.__downstream_timing.start()
 
         self.n += 1
         print_stats = self.n%self.every == 0
@@ -24,6 +31,9 @@ class PrintProfilingStats(BatchFilter):
         if not print_stats:
             return
 
+        total_upstream_time = self.__upstream_timing.elapsed()
+        self.__upstream_timing = Timing(self)
+
         span_start, span_end = self.accumulated_stats.span()
 
         stats = "\n"
@@ -31,11 +41,9 @@ class PrintProfilingStats(BatchFilter):
         stats += "===============\n"
         stats += "\n"
         stats += str(self.accumulated_stats)
-        if self.__last_span_end != 0:
-            time_since_last_span = span_start - self.__last_span_end
-            stats += "Time spend downstream          : %.2f\n"%time_since_last_span
+        stats += "Time span profiled   : %.2f\n"%self.accumulated_stats.span_time()
+        stats += "Time spent upstream  : %.2f\n"%total_upstream_time
         stats += "\n"
         logger.info(stats)
 
         self.accumulated_stats = ProfilingStats()
-        self.__last_span_end = span_end
