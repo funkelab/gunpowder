8dfe850c213de33a50ab208f3fa115c2f5376469
Jan Funke <jfunke@iri.upc.edu>
2017-07-13 07:37:26 -0400

    Ensure docker image is up-to-date in examples/fib25

--- examples/fib25/train.sh
+++ examples/fib25/train.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+CONTAINER=funkey/gunpowder:v0.3-prerelease
+
 NAME=$(basename "$PWD")
 
 nvidia-docker rm -f $NAME
@@ -9,11 +11,13 @@ USER_HOME=${HOME}
 
 echo "Starting as user ${USER_ID} with home ${HOME}"
 
+nvidia-docker pull ${CONTAINER}
+
 NV_GPU=1 nvidia-docker run --rm \
     -u ${USER_ID} \
     -e HOME=${USER_HOME} \
     -v ${PWD}:/run \
     -w /run \
     --name ${NAME} \
-    funkey/gunpowder:v0.2 \
+    ${CONTAINER} \
     python -u train.py 400000 0
