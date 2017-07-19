b5248b581363f4e87762c49147731ff13a4f27f8
juliabuhmann <juliab@ini.phys.ethz.ch>
2017-07-19 12:42:21 +0200

    Make binarymap node use cleaner rasterize function

--- gunpowder/nodes/add_gt_binary_map_points.py
+++ gunpowder/nodes/add_gt_binary_map_points.py
@@ -6,21 +6,27 @@ from scipy import ndimage
 from .batch_filter import BatchFilter
 from gunpowder.coordinate import Coordinate
 from gunpowder.volume import Volume, VolumeTypes
-from gunpowder.points import PointsTypes
+from gunpowder.points import PointsTypes, RasterizationSetting, enlarge_binary_map
 
 
 logger = logging.getLogger(__name__)
 
 class AddGtBinaryMapOfPoints(BatchFilter):
     ''' Create binary map for points of given PointsType in batch and add it as volume to batch '''
-    def __init__(self, pointstype_to_volumetypes):
+    def __init__(self, pointstype_to_volumetypes, pointstype_to_rastersettings=None):
         ''' Add binary map of given PointsType as volume to batch.
         Args:
            pointstype_to_volumetypes: dict, e.g. {PointsType.PRESYN: VolumeTypes.GT_BM_PRESYN} creates a binary map
                                       of points in PointsType.PRESYN and adds the created binary map
-                                      as a volume of type VolumeTypes.GT_BM_PRESYN to the batch if requested. 
+                                      as a volume of type VolumeTypes.GT_BM_PRESYN to the batch if requested.
+           pointstype_to_rastersettings: dict. indicating which kind of rasterization to use for specific pointstype.
+                                    Dict maps PointsType to instance of points.RasterizationSetting
         '''
         self.pointstype_to_volumetypes = pointstype_to_volumetypes
+        if pointstype_to_rastersettings is None:
+            self.pointstype_to_rastersettings = {}
+        else:
+            self.pointstype_to_rastersettings = pointstype_to_rastersettings
         self.skip_next = False
 
 
@@ -64,27 +70,31 @@ class AddGtBinaryMapOfPoints(BatchFilter):
 
         for nr, (points_type, volume_type) in enumerate(self.pointstype_to_volumetypes.items()):
             if volume_type in request.volumes:
-                binary_map = self.__get_binary_map(batch, request, points_type, volume_type, points=batch.points[points_type],
-                                                   marker='gaussian')
+                binary_map = self.__get_binary_map(batch, request, points_type, volume_type, points=batch.points[points_type])
                 resolution = batch.points[points_type].resolution
                 batch.volumes[volume_type] = Volume(data=binary_map,
                                                     roi = request.volumes[volume_type],
                                                     resolution = resolution)
 
 
-    def __get_binary_map(self, batch, request, points_type, volume_type, points, marker='gaussian'):
+    def __get_binary_map(self, batch, request, points_type, volume_type, points):
         """ requires given point locations to lie within to current bounding box already, because offset of batch is wrong"""
 
         shape_bm_volume  = request.volumes[volume_type].get_shape()
         offset_bm_volume = request.volumes[volume_type].get_offset()
         binary_map       = np.zeros(shape_bm_volume, dtype='uint8')
 
+        if points_type in self.pointstype_to_rastersettings:
+            raster_setting = self.pointstype_to_rastersettings[points_type]
+        else:
+            raster_setting = RasterizationSetting()
+        marker = raster_setting.marker_type
         for loc_id in points.data.keys():
             # check if location lies inside bounding box
             if request.volumes[volume_type].contains(Coordinate(batch.points[points_type].data[loc_id].location)):
                 shifted_loc = batch.points[points_type].data[loc_id].location - np.asarray(offset_bm_volume)
                 shifted_loc = shifted_loc.astype(np.int32)
-                if marker == 'point':
+                if marker == 'blob':
                     binary_map[[[loc] for loc in shifted_loc]] = 1
                 elif marker == 'gaussian':
                     marker_size = 1
@@ -96,18 +106,19 @@ class AddGtBinaryMapOfPoints(BatchFilter):
                     # resulting in a binary map which is OFF at those locations instead of ON.
                     binary_map[marker_locs] = 255
 
-        # return mask where location is marked as a single point
-        if marker == 'point':
+
+        # return mask where location is marked as a blob. Set marker_size_voxel to 0, if you require a single point.
+        if marker == 'blob':
+            binary_map = enlarge_binary_map(binary_map, marker_size_voxel=raster_setting.marker_size_voxel,
+                               marker_size_physical=raster_setting.marker_size_physical,
+                               voxel_size=batch.points[points_type].resolution)
             return binary_map
 
-        # return mask where location is marked as a gaussian 'blob'
+        # return mask where location is marked as a gaussian 'blob', produces more square like blobs and does not
+        # take into account anisotropy of the data.
         elif marker == 'gaussian':
             binary_map = ndimage.filters.gaussian_filter(binary_map, sigma=1.)
             binary_map_gaussian = np.zeros_like(binary_map, dtype='uint8')
             binary_map_gaussian[np.nonzero(binary_map)] = 1
-
-            # from scipy import ndimage
-            # binary_map_gaussian = (ndimage.morphology.binary_dilation(binary_map, iterations=5)*255.).astype('uint8')
-
             return binary_map_gaussian
 
