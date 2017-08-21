492e42d6187c29f050668873aa4d37360e4b84ed
Jan Funke <jfunke@iri.upc.edu>
2017-08-21 11:00:38 -0400

    Rewrite ProviderSpec to use VolumeSpecs and PointsSpecs

--- gunpowder/provider_spec.py
+++ gunpowder/provider_spec.py
@@ -1,33 +1,72 @@
 from .freezable import Freezable
-from .roi import Roi
 
 class ProviderSpec(Freezable):
-    '''A possibly partial specification of a provider.
+    '''A collection of (possibly partial) :class:`VolumeSpec`s and 
+    :class:`PointsSpec`s describing a :class:`BatchProvider`'s offered volumes 
+    and points.
+
+    Args:
+
+        volume_specs (dict): A dictionary from :class:`VolumeType` to :class:`VolumeSpec`.
+
+        points_specs (dict): A dictionary from :class:`PointsType` to :class:`PointsSpec`.
     '''
 
-    def __init__(self):
+    def __init__(self, volume_specs=None, points_specs=None):
 
-        self.volumes = {}
-        self.points  = {}
+        if volume_specs is None:
+            self.volume_specs = {}
+        else:
+            self.volume_specs = volume_specs
+
+        if points_specs is None:
+            self.points_specs = {}
+        else:
+            self.points_specs = points_specs
+
         self.freeze()
 
     def get_total_roi(self):
-        '''Get the union of all the provided volume ROIs.'''
+        '''Get the union of all the ROIs.'''
 
         total_roi = None
-        for collection_type in [self.volumes, self.points]:
-            for (type, roi) in collection_type.items():
+        for specs_type in [self.volume_specs, self.points_specs]:
+            for (type, spec) in specs_type.items():
                 if total_roi is None:
-                    total_roi = roi
+                    total_roi = spec.roi
                 else:
-                    total_roi = total_roi.union(roi)
+                    total_roi = total_roi.union(spec.roi)
             return total_roi
 
+    def get_common_roi(self):
+        ''' Get the intersection of all the requested ROIs.'''
+
+        common_roi = None
+        for specs_type in [self.volume_specs, self.points_specs]:
+            for (type, spec) in specs_type.items():
+                if common_roi is None:
+                    common_roi = spec.roi
+                else:
+                    common_roi = common_roi.intersect(spec.roi)
+
+        return common_roi
+
+    def __eq__(self, other):
+
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __ne__(self, other):
+
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
 
     def __repr__(self):
 
         r = ""
-        for collection_type in [self.volumes, self.points]:
-            for (type, roi) in collection_type.items():
-                r += "%s: %s\n"%(type, roi)
+        for specs_type in [self.volume_specs, self.points_specs]:
+            for (type, spec) in specs_type.items():
+                r += "%s: %s\n"%(type, spec)
         return r
