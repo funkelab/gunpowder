99024a67789b1045f5bba264311cc57054fc4c99
Jan Funke <jfunke@iri.upc.edu>
2017-08-22 05:45:31 -0400

    Perform request and batch consistency check in BatchProvider

--- gunpowder/nodes/batch_provider.py
+++ gunpowder/nodes/batch_provider.py
@@ -21,9 +21,6 @@ class BatchProvider(object):
     subclassing :class:`BatchFilter` instead.
     '''
 
-    def __init__(self):
-        self.spec = ProviderSpec()
-
     def add_upstream_provider(self, provider):
         self.get_upstream_providers().append(provider)
         return provider
@@ -55,16 +52,20 @@ class BatchProvider(object):
     def add_volume_spec(self, volume_type, volume_spec):
         '''Introduce a new volume spec provided by this `BatchFilter`.
         '''
-        self.get_spec().volume_specs[volume_type] = volume_spec
+        self.get_spec().volume_specs[volume_type] = copy.deepcopy(volume_spec)
+        logger.debug("%s provides new %s with %s"%(self.name(), volume_type, volume_spec))
 
     def add_points_spec(self, points_type, points_spec):
         '''Introduce a new points spec provided by this `BatchFilter`.
         '''
-        self.get_spec().points_specs[points_type] = points_spec
+        self.get_spec().points_specs[points_type] = copy.deepcopy(points_spec)
+        logger.debug("%s provides new %s with %s"%(self.name(), points_type, points_spec))
 
     def get_spec(self):
         '''Get the :class:`ProviderSpec` of this `BatchProvider`.
         '''
+        if not hasattr(self, 'spec'):
+            self.spec = ProviderSpec()
         return self.spec
 
     def request_batch(self, request):
@@ -76,49 +77,102 @@ class BatchProvider(object):
                 partial) :class:`VolumeSpec`s and :class:`PointsSpec`s.
         '''
 
-        logger.debug("%s got request %s"%(type(self).__name__,request))
+        logger.debug("%s got request %s"%(self.name(),request))
 
-        upstream_request = copy.deepcopy(request)
-        batch = self.provide(upstream_request)
+        self.check_request_consistency(request)
 
-        for (volume_type, spec) in request.volume_specs.items():
-            assert volume_type in batch.volumes, "%s requested, but %s did not provide it."%(volume_type,type(self).__name__)
+        batch = self.provide(copy.deepcopy(request))
+
+        self.check_batch_consistency(batch, request)
+
+        logger.debug("%s provides %s"%(self.name(),batch))
+
+        return batch
+
+    def check_request_consistency(self, request):
+
+        spec = self.get_spec()
+
+        for (volume_type, request_spec) in request.volume_specs.items():
+
+            assert volume_type in spec.volume_specs, "%s: Asked for %s which this node does not provide"%(self.name(), volume_type)
+
+            provided_spec = spec.volume_specs[volume_type]
+
+            provided_roi = provided_spec.roi
+            request_roi = request_spec.roi
+
+            if provided_roi is not None:
+                assert provided_roi.contains(request_roi), "%s: %s's ROI %s outside of my ROI %s"%(self.name(), volume_type, request_roi, provided_roi)
+
+            if request_spec.voxel_size is not None:
+                assert provided_spec.voxel_size == request_spec.voxel_size, "%s: voxel size %s requested for %s, but this node provides %s"%(
+                        request_spec.voxel_size,
+                        volume_type,
+                        provided_spec.voxel_size)
+
+            for d in range(request_roi.dims()):
+                assert request_roi.get_shape()[d]%provided_spec.voxel_size[d] == 0, \
+                        "in request %s, dimension %d of request %s is not a multiple of voxel_size %d"%(
+                                request,
+                                d,
+                                volume_type,
+                                provided_spec.voxel_size[d])
+
+        for (points_type, request_spec) in request.points_specs.items():
+
+            assert points_type in spec.points_specs, "%s: Asked for %s which this node does not provide"%(self.name(), points_type)
+
+            provided_roi = spec.points_specs[points_type].roi
+            request_roi = request_spec.roi
+
+            if provided_roi is not None:
+                assert provided_roi.contains(request_roi), "%s: %s's ROI %s outside of my ROI %s"%(self.name(), points_type, request_roi, provided_roi)
+
+    def check_batch_consistency(self, batch, request):
+
+        for (volume_type, request_spec) in request.volume_specs.items():
+            assert volume_type in batch.volumes, "%s requested, but %s did not provide it."%(volume_type,self.name())
             volume = batch.volumes[volume_type]
-            assert volume.spec.roi == spec.roi, "%s ROI %s requested, but ROI %s provided by %s."%(
+            assert volume.spec.roi == request_spec.roi, "%s ROI %s requested, but ROI %s provided by %s."%(
                     volume_type,
-                    spec.roi,
+                    request_spec.roi,
                     volume.spec.roi,
-                    type(self).__name__
+                    self.name()
             )
             # ensure that the spatial dimensions are the same (other dimensions 
             # on top are okay, e.g., for affinities)
-            dims = spec.roi.dims()
+            dims = request_spec.roi.dims()
             data_shape = Coordinate(volume.data.shape[-dims:])
-            assert data_shape == spec.roi.get_shape()/spec.voxel_size, "%s ROI %s requested, but size of volume is %s*%s=%s provided by %s."%(
+            voxel_size = self.get_spec().volume_specs[volume_type].voxel_size
+            assert data_shape == request_spec.roi.get_shape()/voxel_size, "%s ROI %s requested, but size of volume is %s*%s=%s provided by %s."%(
                     volume_type,
-                    spec.roi,
+                    request_spec.roi,
                     data_shape,
-                    spec.voxel_size,
-                    data_shape*spec.voxel_size,
-                    type(self).__name__
+                    voxel_size,
+                    data_shape*voxel_size,
+                    self.name()
             )
 
-        for (points_type, spec) in request.points_specs.items():
-            assert points_type in batch.points, "%s requested, but %s did not provide it."%(points_type,type(self).__name__)
+        for (points_type, request_spec) in request.points_specs.items():
+            assert points_type in batch.points, "%s requested, but %s did not provide it."%(points_type,self.name())
             points = batch.points[points_type]
-            assert points.spec.roi == spec.roi, "%s ROI %s requested, but ROI %s provided by %s."%(
+            assert points.spec.roi == request_spec.roi, "%s ROI %s requested, but ROI %s provided by %s."%(
                                             points_type,
-                                            spec.roi,
+                                            request_spec.roi,
                                             points.spec.roi,
-                                            type(self).__name__)
-
-        logger.debug("%s provides %s"%(type(self).__name__,batch))
-
-        return batch
+                                            self.name())
 
     def provide(self, request):
         '''To be implemented in subclasses.
 
         Called with a batch request. Should return the requested batch.
         '''
-        raise NotImplementedError("Class %s does not implement 'provide'"%type(self).__name__)
+        raise NotImplementedError("Class %s does not implement 'provide'"%self.name())
+
+    def name(self):
+        return type(self).__name__
+
+    def __repr__(self):
+
+        return self.name() + ": " + str(self.get_spec())
