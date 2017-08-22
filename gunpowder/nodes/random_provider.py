779e82b7596e0fd72cd81c42f28655555f36daa9
Jan Funke <jfunke@iri.upc.edu>
2017-08-22 05:47:24 -0400

    Rewrite RandomProvider to use {Volume,Points}Spec

--- gunpowder/nodes/random_provider.py
+++ gunpowder/nodes/random_provider.py
@@ -9,26 +9,30 @@ class RandomProvider(BatchProvider):
 
     def setup(self):
 
-        self.spec = None
         assert len(self.get_upstream_providers()) > 0, "at least one batch provider needs to be added to the RandomProvider"
+
+        common_spec = None
 
         # advertise volume_types only if all upstream providers have them
         for provider in self.get_upstream_providers():
-            if self.spec is None:
-                self.spec = copy.deepcopy(provider.get_spec())
+
+            if common_spec is None:
+                common_spec = copy.deepcopy(provider.get_spec())
             else:
-                my_volume_types = list(self.spec.volumes.keys())
+                my_volume_types = list(common_spec.volume_specs.keys())
                 for volume_type in my_volume_types:
-                    if volume_type not in provider.get_spec().volumes:
-                        del self.spec.volumes[volume_type]
+                    if volume_type not in provider.get_spec().volume_specs:
+                        del common_spec.volume_specs[volume_type]
 
-                my_points_types = list(self.spec.points.keys())
+                my_points_types = list(common_spec.points_specs.keys())
                 for points_type in my_points_types:
-                    if points_type not in provider.get_spec().points:
-                        del self.spec.points[points_type]
+                    if points_type not in provider.get_spec().points_specs:
+                        del common_spec.points_specsc[points_type]
 
-    def get_spec(self):
-        return self.spec
+        for volume_type, spec in common_spec.volume_specs.items():
+            self.add_volume_spec(volume_type, spec)
+        for points_type, spec in common_spec.points_specs.items():
+            self.add_points_spec(points_type, spec)
 
     def provide(self, request):
         return random.choice(self.get_upstream_providers()).request_batch(request)
