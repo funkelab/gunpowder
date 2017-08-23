cfaab873c70be6adfdc52b46d31b5748ac92e2a2
Jan Funke <jfunke@iri.upc.edu>
2017-08-23 09:44:06 -0400

    Adjust PrepareMalis for {Volume,Points}Spec

--- gunpowder/nodes/prepare_malis.py
+++ gunpowder/nodes/prepare_malis.py
@@ -2,8 +2,8 @@ import copy
 import logging
 import numpy as np
 
-from .batch_filter import BatchFilter
 from gunpowder.volume import Volume, VolumeTypes
+from .batch_filter import BatchFilter
 
 logger = logging.getLogger(__name__)
 
@@ -14,14 +14,18 @@ class PrepareMalis(BatchFilter):
 
         labels_volume_type(:class:`VolumeType`): The label volume to use.
 
-        ignore_volume_type(:class:`VolumeType`): An ignore mask to use, if it 
+        ignore_volume_type(:class:`VolumeType`): An ignore mask to use, if it
             appears in the batch.
 
-        malis_comp_volume_type(:class:`VolumeType`): The malis component volume 
+        malis_comp_volume_type(:class:`VolumeType`): The malis component volume
             to generate.
     '''
 
-    def __init__(self, labels_volume_type=None, ignore_volume_type=None, malis_comp_volume_type=None):
+    def __init__(
+            self,
+            labels_volume_type=None,
+            ignore_volume_type=None,
+            malis_comp_volume_type=None):
 
         if labels_volume_type is None:
             labels_volume_type = VolumeTypes.GT_LABELS
@@ -36,25 +40,25 @@ class PrepareMalis(BatchFilter):
         self.skip_next = False
 
     def setup(self):
-        self.upstream_spec = self.get_upstream_provider().get_spec()
-        self.spec = copy.deepcopy(self.upstream_spec)
 
-        # only give warning that GT_LABELS is missing, in prepare() when checked that node is not skipped
-        if self.labels_volume_type in self.spec.volumes:
-            self.spec.volumes[self.malis_comp_volume_type] = self.spec.volumes[self.labels_volume_type]
-
-    def get_spec(self):
-        return self.spec
+        # only give warning that GT_LABELS is missing, in prepare() when
+        # checked that node is not skipped
+        if self.labels_volume_type in self.spec:
+            spec = self.spec[self.labels_volume_type].copy()
+            self.provides(self.malis_comp_volume_type, spec)
 
     def prepare(self, request):
 
         # MALIS_COMP_LABEL has to be in request for PrepareMalis to run
-        if not self.malis_comp_volume_type in request.volumes:
-            logger.warn("no {} requested, will do nothing".format(self.malis_comp_volume_type))
+        if not self.malis_comp_volume_type in request:
+            logger.warn("no %s requested, will do nothing", self.malis_comp_volume_type)
             self.skip_next = True
         else:
-            assert self.labels_volume_type in request.volumes, "PrepareMalis requires GT_LABELS, but they are not in request"
-            del request.volumes[self.malis_comp_volume_type]
+            assert self.labels_volume_type in request, ("PrepareMalis requires "
+                                                        "%s, but they are not "
+                                                        "in request"% \
+                                                        self.labels_volume_type)
+            del request[self.malis_comp_volume_type]
 
     def process(self, batch, request):
 
@@ -71,14 +75,18 @@ class PrepareMalis(BatchFilter):
         if self.ignore_volume_type in batch.volumes:
 
             gt_neg_pass = np.array(gt_labels.data)
-            gt_neg_pass[batch.volumes[self.ignore_volume_type].data==0] = next_id
+            gt_neg_pass[
+                batch.volumes[self.ignore_volume_type].data == 0] = next_id
 
         else:
 
             gt_neg_pass = gt_pos_pass
 
-        batch.volumes[self.malis_comp_volume_type] = Volume(data=np.array([gt_neg_pass, gt_pos_pass]),
-                                                             roi=request.volumes[self.labels_volume_type])
+        spec = self.spec[self.malis_comp_volume_type].copy()
+        spec.roi = request[self.labels_volume_type].roi
+        batch.volumes[self.malis_comp_volume_type] = Volume(
+            np.array([gt_neg_pass, gt_pos_pass]),
+            spec)
 
         # Why don't we update gt_affinities in the same way?
         # -> not needed
