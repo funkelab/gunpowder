5f7233eae0140b657d3637fed617dda6997d7bdd
Jan Funke <jfunke@iri.upc.edu>
2017-08-29 11:36:57 -0400

    Update TestTensorflowTrain

--- tests/cases/tensorflow_train.py
+++ tests/cases/tensorflow_train.py
@@ -1,35 +1,42 @@
-from .provider_test import ProviderTest
+import numpy as np
 from gunpowder import *
 from gunpowder.tensorflow import Train
 from gunpowder.ext import tensorflow as tf
+from .provider_test import ProviderTest
 
-register_volume_type(VolumeType('A', interpolate=True))
-register_volume_type(VolumeType('B', interpolate=True))
-register_volume_type(VolumeType('C', interpolate=True))
-VolumeTypes.A.voxel_size = (1,1)
-VolumeTypes.B.voxel_size = (1,1)
-VolumeTypes.C.voxel_size = (1,1)
+register_volume_type('A')
+register_volume_type('B')
+register_volume_type('C')
 
 class TestTensorflowTrainSource(BatchProvider):
 
-    def get_spec(self):
+    def setup(self):
 
-        spec = ProviderSpec()
-        spec.volumes[VolumeTypes.A] = Roi((0,0), (2,2))
-        spec.volumes[VolumeTypes.B] = Roi((0,0), (2,2))
-
-        return spec
+        spec = VolumeSpec(
+            roi=Roi((0, 0), (2, 2)),
+            dtype=np.float32,
+            interpolatable=True,
+            voxel_size=(1, 1))
+        self.provides(VolumeTypes.A, spec)
+        self.provides(VolumeTypes.B, spec)
 
     def provide(self, request):
 
         batch = Batch()
 
+        spec = self.spec[VolumeTypes.A]
+        spec.roi = request[VolumeTypes.A].roi
+
         batch.volumes[VolumeTypes.A] = Volume(
-                    [[0,1],[2,3]],
-                    request.volumes[VolumeTypes.A])
+            np.array([[0, 1], [2, 3]], dtype=np.float32),
+            spec)
+
+        spec = self.spec[VolumeTypes.B]
+        spec.roi = request[VolumeTypes.B].roi
+
         batch.volumes[VolumeTypes.B] = Volume(
-                    [[0,1],[2,3]],
-                    request.volumes[VolumeTypes.B])
+            np.array([[0, 1], [2, 3]], dtype=np.float32),
+            spec)
 
         return batch
 
@@ -38,8 +45,8 @@ class TestTensorflowTrain(ProviderTest):
     def test_output(self):
 
         # create a tf graph
-        a = tf.placeholder(tf.float32, shape=(2,2))
-        b = tf.placeholder(tf.float32, shape=(2,2))
+        a = tf.placeholder(tf.float32, shape=(2, 2))
+        b = tf.placeholder(tf.float32, shape=(2, 2))
         v = tf.Variable(1, dtype=tf.float32)
         c = a*b*v
 
@@ -52,18 +59,21 @@ class TestTensorflowTrain(ProviderTest):
 
         source = TestTensorflowTrainSource()
         pipeline = source + Train(
-                optimizer = optimizer,
-                loss = loss,
-                inputs = {VolumeTypes.A: a, VolumeTypes.B: b},
-                outputs = {VolumeTypes.C: c},
-                gradients = {})
+            optimizer=optimizer,
+            loss=loss,
+            inputs={VolumeTypes.A: a, VolumeTypes.B: b},
+            outputs={VolumeTypes.C: c},
+            gradients={})
 
-        request = BatchRequest({
-            VolumeTypes.A: source.get_spec().volumes[VolumeTypes.A],
-            VolumeTypes.B: source.get_spec().volumes[VolumeTypes.B]
-        })
 
         with build(pipeline):
+
+            request = BatchRequest({
+                VolumeTypes.A: VolumeSpec(roi=Roi((0, 0), (2, 2))),
+                VolumeTypes.B: VolumeSpec(roi=Roi((0, 0), (2, 2))),
+                VolumeTypes.C: VolumeSpec(roi=Roi((0, 0), (2, 2))),
+            })
+
             batch = pipeline.request_batch(request)
 
         self.assertAlmostEqual(batch.loss, 9.8994951)
