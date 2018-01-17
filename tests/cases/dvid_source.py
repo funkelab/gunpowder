b4fa9191f6b46df4c213d29ef890ebbeae6d007b
Jan Funke <jfunke@iri.upc.edu>
2018-01-17 11:11:59 +0100

    Add test for DvidSource

---
+++ tests/cases/dvid_source.py
@@ -0,0 +1,40 @@
+from .provider_test import ProviderTest
+from gunpowder import *
+import numpy as np
+
+class TestDvidSource(ProviderTest):
+
+    def test_output_3d(self):
+
+        # create array keys
+        raw = ArrayKey('RAW')
+        seg = ArrayKey('SEG')
+        mask = ArrayKey('MASK')
+
+        source = DvidSource(
+            'slowpoke1',
+            32768,
+            '2ad1d8f0f172425c9f87b60fd97331e6',
+            datasets = {
+                raw: 'grayscale',
+                seg: 'groundtruth'
+            },
+            masks = {
+                mask: 'seven_column'
+            }
+        )
+
+        with build(source):
+
+            batch = source.request_batch(
+                BatchRequest({
+                    raw: ArraySpec(roi=Roi((0, 0, 0), (100, 100, 100))),
+                    seg: ArraySpec(roi=Roi((0, 0, 0), (100, 100, 100))),
+                    mask: ArraySpec(roi=Roi((0, 0, 0), (100, 100, 100)))
+                })
+            )
+
+            self.assertTrue(batch.arrays[raw].spec.interpolatable)
+            self.assertFalse(batch.arrays[seg].spec.interpolatable)
+            self.assertFalse(batch.arrays[mask].spec.interpolatable)
+
