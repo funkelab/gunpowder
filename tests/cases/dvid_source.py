6a22faf5c4ffb009a793d47eddad628032e6703c
Jan Funke <jfunke@iri.upc.edu>
2018-01-17 12:16:40 +0100

    Update test for DvidSource

--- tests/cases/dvid_source.py
+++ tests/cases/dvid_source.py
@@ -11,26 +11,36 @@ class TestDvidSource(ProviderTest):
         seg = ArrayKey('SEG')
         mask = ArrayKey('MASK')
 
-        source = DvidSource(
-            'slowpoke1',
-            32768,
-            '2ad1d8f0f172425c9f87b60fd97331e6',
-            datasets = {
-                raw: 'grayscale',
-                seg: 'groundtruth'
-            },
-            masks = {
-                mask: 'seven_column'
-            }
+        pipeline = (
+            DvidSource(
+                'slowpoke1',
+                32768,
+                '2ad1d8f0f172425c9f87b60fd97331e6',
+                datasets = {
+                    raw: 'grayscale',
+                    seg: 'groundtruth'
+                },
+                masks = {
+                    mask: 'seven_column'
+                }
+            ) +
+            Snapshot(
+                {
+                    raw: '/volumes/raw',
+                    seg: '/volumes/labels/neuron_ids',
+                    mask: '/volumes/labels/mask'
+                },
+                output_filename = 'dvid_source_test.hdf'
+            )
         )
 
-        with build(source):
+        with build(pipeline):
 
-            batch = source.request_batch(
+            batch = pipeline.request_batch(
                 BatchRequest({
-                    raw: ArraySpec(roi=Roi((0, 0, 0), (80, 80, 80))),
-                    seg: ArraySpec(roi=Roi((0, 0, 0), (80, 80, 80))),
-                    mask: ArraySpec(roi=Roi((0, 0, 0), (80, 80, 80)))
+                    raw: ArraySpec(roi=Roi((33000, 15000, 20000), (32000, 8, 80))),
+                    seg: ArraySpec(roi=Roi((33000, 15000, 20000), (32000, 8, 80))),
+                    mask: ArraySpec(roi=Roi((33000, 15000, 20000), (32000, 8, 80)))
                 })
             )
 
@@ -38,3 +48,6 @@ class TestDvidSource(ProviderTest):
             self.assertFalse(batch.arrays[seg].spec.interpolatable)
             self.assertFalse(batch.arrays[mask].spec.interpolatable)
 
+            self.assertEqual(batch.arrays[raw].spec.voxel_size, (8, 8, 8))
+            self.assertEqual(batch.arrays[seg].spec.voxel_size, (8, 8, 8))
+            self.assertEqual(batch.arrays[mask].spec.voxel_size, (8, 8, 8))
