fd0932a628f5821cb5daf90aaeb77d0de43d834b
Jan Funke <jfunke@iri.upc.edu>
2018-03-16 15:59:06 -0500

    Add test for Pad

--- tests/cases/__init__.py
+++ tests/cases/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-from .add_boundary_distance_gradients import TestAddBoundaryDistanceGradients
 from .add_affinities import TestAddAffinities
+from .add_boundary_distance_gradients import TestAddBoundaryDistanceGradients
 from .add_vector_map import TestAddVectorMap
 from .balance_labels import TestBalanceLabels
 from .crop import TestCrop
@@ -12,6 +12,7 @@ from .hdf5_source import TestHdf5Source
 from .hdf5_write import TestHdf5Write
 from .merge_provider import TestMergeProvider
 from .normalize import TestNormalize
+from .pad import TestPad
 from .points_keys import TestPointsKeys
 from .precache import TestPreCache
 from .prepare_malis import TestPrepareMalis
