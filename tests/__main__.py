e818cd4954a3f83bbf0411d1e9ffd1436aa6004a
Chris Barnes <barnesc@janelia.hhmi.org>
2018-04-10 12:10:03 -0400

    make tests easier to run

---
+++ tests/__main__.py
@@ -0,0 +1,30 @@
+import unittest
+from argparse import ArgumentParser
+import logging
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='count', default=0,
+                        help='Logging verbosity: repeat for more output')
+    parser.add_argument('--output', '-o',
+                        help='Path to log file (default stderr)')
+    parsed_args = parser.parse_args()
+    log_level = {
+        0: logging.CRITICAL,
+        1: logging.WARNING,
+        2: logging.INFO,
+        3: logging.DEBUG
+    }.get(parsed_args.verbose, logging.DEBUG)
+
+    kwargs = {'level': log_level}
+
+    if parsed_args.output:
+        kwargs['filename'] = parsed_args.output
+        kwargs['filemode'] = 'w'
+
+    logging.basicConfig(**kwargs)
+    from cases import *  # to log any import-related issues
+
+    logging.getLogger().setLevel(log_level)
+
+    unittest.main(verbosity=2 if parsed_args.verbose else 1)
