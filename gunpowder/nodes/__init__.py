19d1b34bc28bfbb3e5df3a252d88a504346542d0
Jan Funke <funkej@janelia.hhmi.org>
2018-07-09 14:32:16 -0400

    Add ZarrWrite

--- gunpowder/nodes/__init__.py
+++ gunpowder/nodes/__init__.py
@@ -33,4 +33,5 @@ from .scan import Scan
 from .simple_augment import SimpleAugment
 from .snapshot import Snapshot
 from .specified_location import SpecifiedLocation
-from .zarr_source import ZarrFile, ZarrSource
+from .zarr_source import ZarrSource
+from .zarr_write import ZarrWrite
