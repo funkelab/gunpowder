64bc193dc3271c16d2336e47d00a86a79a25cd8c
Jan Funke <funkej@janelia.hhmi.org>
2019-07-10 15:18:09 -0400

    Add Points.{crop,merge}

--- gunpowder/points.py
+++ gunpowder/points.py
@@ -1,4 +1,6 @@
 from .freezable import Freezable
+from copy import copy as shallow_copy
+from copy import deepcopy
 import logging
 import numpy as np
 
@@ -26,6 +28,62 @@ class Points(Freezable):
 
     def __repr__(self):
         return "%s, %s"%(self.data, self.spec)
+
+    def crop(self, roi, copy=False):
+        '''Crop this point set to the given ROI.
+        '''
+
+        if copy:
+            cropped = deepcopy(self)
+        else:
+            cropped = shallow_copy(self)
+
+        cropped.data = {
+            k: v
+            for k, v in cropped.data
+            if roi.contains(v.location)
+        }
+
+        return cropped
+
+    def merge(self, points, copy_from_self=False, copy=False):
+        '''Merge these points with another set of points. The resulting points
+        will have the ROI of the larger one.
+
+        This only works if one of the two point sets is contained in the other.
+        In this case, ``points`` will overwrite points with the same ID in
+        ``self`` (unless ``copy_from_self`` is set to ``True``).
+
+        A copy will only be made if necessary or ``copy`` is set to ``True``.
+        '''
+
+        self_roi = self.spec.roi
+        points_roi = points.spec.roi
+
+        assert self_roi.contains(points_roi) or points_roi.contains(self_roi), \
+            "Can not merge point sets that are not contained in each other."
+
+        # make sure self contains points
+        if not self_roi.contains(points_roi):
+            return points.merge(self, not copy_from_self, copy)
+
+        # -> here we know that self contains points
+
+        # simple case, self overwrites all of points
+        if copy_from_self:
+            return self if not copy else deepcopy(self)
+
+        # -> here we know that copy_from_self == False
+
+        # replace points
+        if copy:
+            merged = shallow_copy(self)
+            merged.data.update(points.data)
+        else:
+            merged = deepcopy(self)
+            merged.data.update(deepcopy(points.data))
+
+        return merged
 
 class Point(Freezable):
     '''A point with a location, as stored in :class:`Points`.
