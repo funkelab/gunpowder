591c31b298c559a1289c92cde2a7a1912fb9711e
pattonw <pattonw@hhmi.org>
2020-03-06 13:47:12 -0500

    Black formatting

--- gunpowder/nodes/elastic_augment.py
+++ gunpowder/nodes/elastic_augment.py
@@ -13,8 +13,9 @@ from gunpowder.roi import Roi
 
 logger = logging.getLogger(__name__)
 
+
 class ElasticAugment(BatchFilter):
-    '''Elasticly deform a batch. Requests larger batches upstream to avoid data 
+    """Elasticly deform a batch. Requests larger batches upstream to avoid data 
     loss due to rotation and jitter.
 
     Args:
@@ -65,7 +66,7 @@ class ElasticAugment(BatchFilter):
             dimension. If, for example, your array is indexed as ``(c,y,x)``
             (2D plus channels), you would want to set ``spatial_dims=2`` to
             perform the elastic deformation only on x and y.
-    '''
+    """
 
     def __init__(
         self,
@@ -104,25 +105,26 @@ class ElasticAugment(BatchFilter):
 
         # get the total ROI of all requests
         total_roi = request.get_total_roi()
-        logger.debug("total ROI is %s"%total_roi)
+        logger.debug("total ROI is %s" % total_roi)
 
         # First, get the total ROI of the request in spatial dimensions only.
         # Channels and time don't matter. This is our master ROI.
 
         # get master ROI
         master_roi = Roi(
-            total_roi.get_begin()[-self.spatial_dims:],
-            total_roi.get_shape()[-self.spatial_dims:])
+            total_roi.get_begin()[-self.spatial_dims :],
+            total_roi.get_shape()[-self.spatial_dims :],
+        )
         self.spatial_dims = master_roi.dims()
-        logger.debug("master ROI is %s"%master_roi)
+        logger.debug("master ROI is %s" % master_roi)
 
         # make sure the master ROI aligns with the voxel size
-        master_roi = master_roi.snap_to_grid(self.voxel_size, mode='grow')
-        logger.debug("master ROI aligned with voxel size is %s"%master_roi)
+        master_roi = master_roi.snap_to_grid(self.voxel_size, mode="grow")
+        logger.debug("master ROI aligned with voxel size is %s" % master_roi)
 
         # get master roi in voxels
-        master_roi_voxels = master_roi/self.voxel_size
-        logger.debug("master ROI in voxels is %s"%master_roi_voxels)
+        master_roi_voxels = master_roi / self.voxel_size
+        logger.debug("master ROI in voxels is %s" % master_roi_voxels)
 
         # Second, create a master transformation. This is a transformation that
         # covers all voxels of the all requested ROIs. The master transformation
@@ -130,7 +132,8 @@ class ElasticAugment(BatchFilter):
 
         # create a transformation with the size of the master ROI in voxels
         self.master_transformation = self.__create_transformation(
-            master_roi_voxels.get_shape())
+            master_roi_voxels.get_shape()
+        )
 
         # Third, crop out parts of the master transformation for each of the
         # smaller requested ROIs. Since these ROIs now have to align with the
@@ -149,35 +152,38 @@ class ElasticAugment(BatchFilter):
                 continue
 
             target_roi = Roi(
-                spec.roi.get_begin()[-self.spatial_dims:],
-                spec.roi.get_shape()[-self.spatial_dims:])
-            logger.debug(
-                "downstream request spatial ROI for %s is %s", key, target_roi)
+                spec.roi.get_begin()[-self.spatial_dims :],
+                spec.roi.get_shape()[-self.spatial_dims :],
+            )
+            logger.debug("downstream request spatial ROI for %s is %s", key, target_roi)
 
             # make sure the target ROI aligns with the voxel grid (which might
             # not be the case for points)
-            target_roi = target_roi.snap_to_grid(self.voxel_size, mode='grow')
+            target_roi = target_roi.snap_to_grid(self.voxel_size, mode="grow")
             logger.debug(
                 "downstream request spatial ROI aligned with voxel grid for %s "
-                "is %s", key, target_roi)
+                "is %s",
+                key,
+                target_roi,
+            )
 
             # remember target ROI (this is where the transformation will project
             # to)
             self.target_rois[key] = target_roi
 
             # get ROI in voxels
-            target_roi_voxels = target_roi/self.voxel_size
+            target_roi_voxels = target_roi / self.voxel_size
 
             # get ROI relative to master ROI
             target_roi_in_master_roi_voxels = (
-                target_roi_voxels -
-                master_roi_voxels.get_begin())
+                target_roi_voxels - master_roi_voxels.get_begin()
+            )
 
             # crop out relevant part of transformation for this request
             transformation = np.copy(
                 self.master_transformation[
-                    (slice(None),) +
-                    target_roi_in_master_roi_voxels.get_bounding_box()]
+                    (slice(None),) + target_roi_in_master_roi_voxels.get_bounding_box()
+                ]
             )
             self.transformations[key] = transformation
 
@@ -188,9 +194,9 @@ class ElasticAugment(BatchFilter):
             # reverse
             source_roi_in_master_roi_voxels = self.__get_source_roi(transformation)
             source_roi_voxels = (
-                source_roi_in_master_roi_voxels +
-                master_roi_voxels.get_begin())
-            source_roi = source_roi_voxels*self.voxel_size
+                source_roi_in_master_roi_voxels + master_roi_voxels.get_begin()
+            )
+            source_roi = source_roi_voxels * self.voxel_size
 
             # transformation is still defined on voxels relative to master ROI
             # in voxels (i.e., lowest source coordinate could be 5, but data
@@ -199,13 +205,16 @@ class ElasticAugment(BatchFilter):
             # shift transformation to be indexed relative to beginning of
             # source_roi_voxels
             self.__shift_transformation(
-                -source_roi_in_master_roi_voxels.get_begin(),
-                transformation)
+                -source_roi_in_master_roi_voxels.get_begin(), transformation
+            )
 
             # update upstream request
             spec.roi = Roi(
-                spec.roi.get_begin()[:-self.spatial_dims] + source_roi.get_begin()[-self.spatial_dims:],
-                spec.roi.get_shape()[:-self.spatial_dims] + source_roi.get_shape()[-self.spatial_dims:])
+                spec.roi.get_begin()[: -self.spatial_dims]
+                + source_roi.get_begin()[-self.spatial_dims :],
+                spec.roi.get_shape()[: -self.spatial_dims]
+                + source_roi.get_shape()[-self.spatial_dims :],
+            )
 
             deps[key] = spec
 
@@ -223,25 +232,37 @@ class ElasticAugment(BatchFilter):
             # for arrays, the target ROI and the requested ROI should be the
             # same in spatial coordinates
             assert (
-                self.target_rois[array_key].get_begin() ==
-                request[array_key].roi.get_begin()[-self.spatial_dims:])
+                self.target_rois[array_key].get_begin()
+                == request[array_key].roi.get_begin()[-self.spatial_dims :]
+            ), "Target roi offset {} does not match request roi offset {}".format(
+                self.target_rois[array_key].get_begin(),
+                request[array_key].roi.get_begin()[-self.spatial_dims :],
+            )
+
             assert (
-                self.target_rois[array_key].get_shape() ==
-                request[array_key].roi.get_shape()[-self.spatial_dims:])
+                self.target_rois[array_key].get_shape()
+                == request[array_key].roi.get_shape()[-self.spatial_dims :]
+            ), "Target roi offset {} does not match request roi offset {}".format(
+                self.target_rois[array_key].get_shape(),
+                request[array_key].roi.get_shape()[-self.spatial_dims :],
+            )
 
             # reshape array data into (channels,) + spatial dims
             shape = array.data.shape
-            channel_shape = shape[:-self.spatial_dims]
-            data = array.data.reshape((-1,) + shape[-self.spatial_dims:])
+            channel_shape = shape[: -self.spatial_dims]
+            data = array.data.reshape((-1,) + shape[-self.spatial_dims :])
 
             # apply transformation on each channel
-            data = np.array([
-                augment.apply_transformation(
-                    data[c],
-                    self.transformations[array_key],
-                    interpolate=self.spec[array_key].interpolatable)
-                for c in range(data.shape[0])
-            ])
+            data = np.array(
+                [
+                    augment.apply_transformation(
+                        data[c],
+                        self.transformations[array_key],
+                        interpolate=self.spec[array_key].interpolatable,
+                    )
+                    for c in range(data.shape[0])
+                ]
+            )
 
             data_roi = request[array_key].roi/self.spec[array_key].voxel_size
             array.data = data.reshape(channel_shape + data_roi.get_shape()[-self.spatial_dims:])
@@ -275,21 +296,18 @@ class ElasticAugment(BatchFilter):
                 location = node.location - graph.spec.roi.get_begin()
                 logger.debug("relative to upstream ROI: %s", location)
 
-                # get spatial coordinates of node in voxels
-                location_voxels = location[-self.spatial_dims:]/self.voxel_size
-                logger.debug(
-                    "relative to upstream ROI in voxels: %s",
-                    location_voxels)
+                # get spatial coordinates of point in voxels
+                location_voxels = location[-self.spatial_dims :] / self.voxel_size
 
                 # get projected location in transformation data space, this
                 # yields voxel coordinates relative to target ROI
                 projected_voxels = self.__project(
-                    self.transformations[graph_key],
-                    location_voxels)
+                    self.transformations[points_key], location_voxels
+                )
 
                 logger.debug(
-                    "projected in voxels, relative to target ROI: %s",
-                    projected_voxels)
+                    "projected in voxels, relative to target ROI: %s", projected_voxels
+                )
 
                 if projected_voxels is None:
                     logger.debug("point outside of target, skipping")
@@ -297,7 +315,7 @@ class ElasticAugment(BatchFilter):
                     continue
 
                 # convert to world units (now in float again)
-                projected = projected_voxels*np.array(self.voxel_size)
+                projected = projected_voxels * np.array(self.voxel_size)
 
                 logger.debug(
                     "projected in world units, relative to target ROI: %s",
@@ -344,25 +362,25 @@ class ElasticAugment(BatchFilter):
     def __create_transformation(self, target_shape):
 
         transformation = augment.create_identity_transformation(
-                target_shape,
-                subsample=self.subsample)
+            target_shape, subsample=self.subsample
+        )
         if sum(self.jitter_sigma) > 0:
             transformation += augment.create_elastic_transformation(
-                    target_shape,
-                    self.control_point_spacing,
-                    self.jitter_sigma,
-                    subsample=self.subsample)
-        rotation = random.random()*self.rotation_max_amount + self.rotation_start
+                target_shape,
+                self.control_point_spacing,
+                self.jitter_sigma,
+                subsample=self.subsample,
+            )
+        rotation = random.random() * self.rotation_max_amount + self.rotation_start
         if rotation != 0:
             transformation += augment.create_rotation_transformation(
-                    target_shape,
-                    rotation,
-                    subsample=self.subsample)
+                target_shape, rotation, subsample=self.subsample
+            )
 
         if self.subsample > 1:
             transformation = augment.upscale_transformation(
-                    transformation,
-                    target_shape)
+                transformation, target_shape
+            )
 
         if self.prob_slip + self.prob_shift > 0:
             self.__misalign(transformation)
@@ -432,8 +450,8 @@ class ElasticAugment(BatchFilter):
         return missing_points
 
     def __project(self, transformation, location):
-        '''Find the projection of location given by transformation. Returns None
-        if projection lies outside of transformation.'''
+        """Find the projection of location given by transformation. Returns None
+        if projection lies outside of transformation."""
 
         dims = len(location)
 
@@ -443,7 +461,7 @@ class ElasticAugment(BatchFilter):
             diff[d] -= location[d]
 
         # square
-        diff2 = diff*diff
+        diff2 = diff * diff
 
         # sum
         dist = diff2.sum(axis=0)
@@ -479,7 +497,7 @@ class ElasticAugment(BatchFilter):
                 pos_dist = pos_source[d] - center_source[d]
                 loc_dist = location[d] - center_source[d]
                 if pos_dist != 0:
-                    pos_u = loc_dist/pos_dist
+                    pos_u = loc_dist / pos_dist
                 else:
                     pos_u = 0
 
@@ -489,7 +507,7 @@ class ElasticAugment(BatchFilter):
                 neg_dist = neg_source[d] - center_source[d]
                 loc_dist = location[d] - center_source[d]
                 if neg_dist != 0:
-                    neg_u = loc_dist/neg_dist
+                    neg_u = loc_dist / neg_dist
                 else:
                     neg_u = 0
 
@@ -502,9 +520,9 @@ class ElasticAugment(BatchFilter):
         return np.array(center_grid, dtype=np.float32)
 
     def __source_at(self, transformation, index):
-        '''Read the source point of a transformation at index.'''
+        """Read the source point of a transformation at index."""
 
-        slices = (slice(None),) + tuple(slice(i, i+1) for i in index)
+        slices = (slice(None),) + tuple(slice(i, i + 1) for i in index)
         return transformation[slices].flatten()
 
     def __get_source_roi(self, transformation):
@@ -512,14 +530,15 @@ class ElasticAugment(BatchFilter):
         dims = transformation.shape[0]
 
         # get bounding box of needed data for transformation
-        bb_min = Coordinate(int(math.floor(transformation[d].min())) for d in range(dims))
-        bb_max = Coordinate(int(math.ceil(transformation[d].max())) + 1 for d in range(dims))
+        bb_min = Coordinate(
+            int(math.floor(transformation[d].min())) for d in range(dims)
+        )
+        bb_max = Coordinate(
+            int(math.ceil(transformation[d].max())) + 1 for d in range(dims)
+        )
 
         # create roi sufficiently large to feed transformation
-        source_roi = Roi(
-                bb_min,
-                bb_max - bb_min
-        )
+        source_roi = Roi(bb_min, bb_max - bb_min)
 
         return source_roi
 
@@ -530,12 +549,13 @@ class ElasticAugment(BatchFilter):
 
     def __misalign(self, transformation):
 
-        assert transformation.shape[0] == 3, (
-            "misalign can only be applied to 3D volumes")
+        assert (
+            transformation.shape[0] == 3
+        ), "misalign can only be applied to 3D volumes"
 
         num_sections = transformation[0].shape[0]
 
-        shifts = [Coordinate((0,0,0))]*num_sections
+        shifts = [Coordinate((0, 0, 0))] * num_sections
         for z in range(num_sections):
 
             r = random.random()
@@ -558,13 +578,25 @@ class ElasticAugment(BatchFilter):
         logger.debug("min/max of transformation: " + str(bb_min) + "/" + str(bb_max))
 
         for z in range(num_sections):
-            transformation[1][z,:,:] += shifts[z][1]
-            transformation[2][z,:,:] += shifts[z][2]
+            transformation[1][z, :, :] += shifts[z][1]
+            transformation[2][z, :, :] += shifts[z][2]
 
         bb_min = tuple(int(math.floor(transformation[d].min())) for d in range(dims))
         bb_max = tuple(int(math.ceil(transformation[d].max())) + 1 for d in range(dims))
-        logger.debug("min/max of transformation after misalignment: " + str(bb_min) + "/" + str(bb_max))
+        logger.debug(
+            "min/max of transformation after misalignment: "
+            + str(bb_min)
+            + "/"
+            + str(bb_max)
+        )
 
     def __random_offset(self):
 
-        return Coordinate((0,) + tuple(self.max_misalign - random.randint(0, 2*int(self.max_misalign)) for d in range(2)))
+        return Coordinate(
+            (0,)
+            + tuple(
+                self.max_misalign - random.randint(0, 2 * int(self.max_misalign))
+                for d in range(2)
+            )
+        )
+
