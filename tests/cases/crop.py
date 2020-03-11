36ae2ae93b678ba4bcf75e77cf95efe18d534b6b
pattonw <pattonw@hhmi.org>
2020-03-11 15:10:41 -0400

    convert crop test to Graphs

--- tests/cases/crop.py
+++ tests/cases/crop.py
@@ -1,62 +1,64 @@
 from .provider_test import ProviderTest
-from gunpowder import *
-import numpy as np
+from gunpowder import (
+    BatchProvider,
+    ArrayKeys,
+    ArraySpec,
+    Roi,
+    GraphKey,
+    GraphKeys,
+    GraphSpec,
+    Crop,
+    build
+)
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class TestSourceCrop(BatchProvider):
-
     def setup(self):
 
         self.provides(
             ArrayKeys.RAW,
-            ArraySpec(
-                roi=Roi((200, 20, 20), (1800, 180, 180)),
-                voxel_size=(20, 2, 2)))
+            ArraySpec(roi=Roi((200, 20, 20), (1800, 180, 180)), voxel_size=(20, 2, 2)),
+        )
 
         self.provides(
-            PointsKeys.PRESYN,
-            PointsSpec(
-                roi=Roi((200, 20, 20), (1800, 180, 180))))
+            GraphKeys.PRESYN, GraphSpec(roi=Roi((200, 20, 20), (1800, 180, 180)))
+        )
 
     def provide(self, request):
         pass
 
 
 class TestCrop(ProviderTest):
-
     def test_output(self):
 
-        cropped_roi_raw    = Roi((400, 40, 40), (1000, 100, 100))
+        cropped_roi_raw = Roi((400, 40, 40), (1000, 100, 100))
         cropped_roi_presyn = Roi((800, 80, 80), (800, 80, 80))
 
-        PointsKey('PRESYN')
+        GraphKey("PRESYN")
 
         pipeline = (
-            TestSourceCrop() +
-            Crop(ArrayKeys.RAW, cropped_roi_raw) +
-            Crop(PointsKeys.PRESYN, cropped_roi_presyn))
+            TestSourceCrop()
+            + Crop(ArrayKeys.RAW, cropped_roi_raw)
+            + Crop(GraphKeys.PRESYN, cropped_roi_presyn)
+        )
 
         with build(pipeline):
 
-            self.assertTrue(
-                pipeline.spec[ArrayKeys.RAW].roi == cropped_roi_raw)
-            self.assertTrue(
-                pipeline.spec[PointsKeys.PRESYN].roi == cropped_roi_presyn)
+            self.assertTrue(pipeline.spec[ArrayKeys.RAW].roi == cropped_roi_raw)
+            self.assertTrue(pipeline.spec[GraphKeys.PRESYN].roi == cropped_roi_presyn)
 
-        pipeline = (
-            TestSourceCrop() +
-            Crop(
-                ArrayKeys.RAW,
-                fraction_negative=(0.25, 0, 0),
-                fraction_positive=(0.25, 0, 0)))
+        pipeline = TestSourceCrop() + Crop(
+            ArrayKeys.RAW,
+            fraction_negative=(0.25, 0, 0),
+            fraction_positive=(0.25, 0, 0),
+        )
         expected_roi_raw = Roi((650, 20, 20), (900, 180, 180))
 
         with build(pipeline):
 
             logger.info(pipeline.spec[ArrayKeys.RAW].roi)
             logger.info(expected_roi_raw)
-            self.assertTrue(
-                pipeline.spec[ArrayKeys.RAW].roi == expected_roi_raw)
+            self.assertTrue(pipeline.spec[ArrayKeys.RAW].roi == expected_roi_raw)
