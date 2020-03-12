b7fd0a721aedce0d8d25da8fa46815974659dda2
pattonw <pattonw@hhmi.org>
2020-03-12 08:51:55 -0400

    update merge provider test case to use Graphs

--- tests/cases/merge_provider.py
+++ tests/cases/merge_provider.py
@@ -1,31 +1,42 @@
 import unittest
-from gunpowder import *
-from gunpowder.points import PointsKeys, Points
+from gunpowder import (
+    GraphSpec,
+    GraphKey,
+    Roi,
+    Coordinate,
+    Batch,
+    BatchProvider,
+    ArrayKeys,
+    ArrayKey,
+    ArraySpec,
+    Array,
+    BatchRequest,
+    MergeProvider,
+    RandomLocation,
+    build,
+)
+from gunpowder.graph import GraphKeys, Graph
 
 import numpy as np
 
-class PointTestSource(BatchProvider):
 
+class PointTestSource(BatchProvider):
     def __init__(self, voxel_size):
         self.voxel_size = voxel_size
 
     def setup(self):
 
-        self.provides(
-            PointsKeys.PRESYN,
-            PointsSpec(roi=Roi((0, 0, 0), (100, 100, 100))))
+        self.provides(GraphKeys.PRESYN, GraphSpec(roi=Roi((0, 0, 0), (100, 100, 100))))
 
     def provide(self, request):
         batch = Batch()
-        roi_points = request[PointsKeys.PRESYN].roi
+        roi_points = request[GraphKeys.PRESYN].roi
 
-        batch.points[PointsKeys.PRESYN] = Points(
-            {},
-            PointsSpec(roi=roi_points))
+        batch.graphs[GraphKeys.PRESYN] = Graph([], [], GraphSpec(roi=roi_points))
         return batch
 
-class ArrayTestSoure(BatchProvider):
 
+class ArrayTestSoure(BatchProvider):
     def __init__(self, voxel_size):
         self.voxel_size = voxel_size
 
@@ -33,29 +44,26 @@ class ArrayTestSoure(BatchProvider):
 
         self.provides(
             ArrayKeys.GT_LABELS,
-            ArraySpec(
-                roi=Roi((0, 0, 0), (100, 100, 100)),
-                voxel_size=self.voxel_size))
+            ArraySpec(roi=Roi((0, 0, 0), (100, 100, 100)), voxel_size=self.voxel_size),
+        )
 
     def provide(self, request):
         roi_array = request[ArrayKeys.GT_LABELS].roi
         data = np.zeros(
-            roi_array.get_shape() /
-            self.spec[ArrayKeys.GT_LABELS].voxel_size)
+            roi_array.get_shape() / self.spec[ArrayKeys.GT_LABELS].voxel_size
+        )
         batch = Batch()
         spec = self.spec[ArrayKeys.GT_LABELS].copy()
         spec.roi = roi_array
-        batch.arrays[ArrayKeys.GT_LABELS] = Array(
-            data,
-            spec)
+        batch.arrays[ArrayKeys.GT_LABELS] = Array(data, spec)
         return batch
 
 
 class TestMergeProvider(unittest.TestCase):
-
     def test_merge_basics(self):
         voxel_size = (1, 1, 1)
-        PointsKey('PRESYN')
+        GraphKey("PRESYN")
+        ArrayKey("GT_LABELS")
         pointssource = PointTestSource(voxel_size)
         arraysource = ArrayTestSoure(voxel_size)
         pipeline = (pointssource, arraysource) + MergeProvider() + RandomLocation()
@@ -63,18 +71,18 @@ class TestMergeProvider(unittest.TestCase):
         with build(pipeline):
             # Check basic merging.
             request = BatchRequest()
-            request.add((PointsKeys.PRESYN), window_request)
+            request.add((GraphKeys.PRESYN), window_request)
             request.add((ArrayKeys.GT_LABELS), window_request)
             batch_res = pipeline.request_batch(request)
             self.assertTrue(ArrayKeys.GT_LABELS in batch_res.arrays)
-            self.assertTrue(PointsKeys.PRESYN in batch_res.points)
+            self.assertTrue(GraphKeys.PRESYN in batch_res.graphs)
 
             # Check that request of only one source also works.
             request = BatchRequest()
-            request.add((PointsKeys.PRESYN), window_request)
+            request.add((GraphKeys.PRESYN), window_request)
             batch_res = pipeline.request_batch(request)
             self.assertFalse(ArrayKeys.GT_LABELS in batch_res.arrays)
-            self.assertTrue(PointsKeys.PRESYN in batch_res.points)
+            self.assertTrue(GraphKeys.PRESYN in batch_res.graphs)
 
         # Check that it fails, when having two sources that provide the same type.
         arraysource2 = ArrayTestSoure(voxel_size)
@@ -82,5 +90,4 @@ class TestMergeProvider(unittest.TestCase):
         with self.assertRaises(AssertionError):
             with build(pipeline_fail):
                 pass
-
 
