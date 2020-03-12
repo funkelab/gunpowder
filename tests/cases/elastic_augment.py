288cbfcced26a267348c5a39da2b95e5191214b0
pattonw <pattonw@hhmi.org>
2020-03-12 09:05:55 -0400

    rename elastic augment test

--- tests/cases/elastic_augment.py
+++ tests/cases/elastic_augment.py
@@ -23,10 +23,10 @@ import numpy as np
 import math
 
 
-class PointTestSource3D(BatchProvider):
+class GraphTestSource3D(BatchProvider):
     def setup(self):
 
-        self.points = [
+        self.vertices = [
             Vertex(id=0, location=np.array([0, 0, 0])),
             Vertex(id=1, location=np.array([0, 10, 0])),
             Vertex(id=2, location=np.array([0, 20, 0])),
@@ -36,7 +36,7 @@ class PointTestSource3D(BatchProvider):
         ]
 
         self.provides(
-            GraphKeys.TEST_POINTS,
+            GraphKeys.TEST_GRAPH,
             GraphSpec(roi=Roi((-100, -100, -100), (200, 200, 200))),
         )
 
@@ -49,7 +49,7 @@ class PointTestSource3D(BatchProvider):
             ),
         )
 
-    def point_to_voxel(self, array_roi, location):
+    def vertex_to_voxel(self, array_roi, location):
 
         # location is in world units, get it into voxels
         location = location / self.spec[ArrayKeys.TEST_LABELS].voxel_size
@@ -63,27 +63,27 @@ class PointTestSource3D(BatchProvider):
 
         batch = Batch()
 
-        roi_points = request[GraphKeys.TEST_POINTS].roi
+        roi_graph = request[GraphKeys.TEST_GRAPH].roi
         roi_array = request[ArrayKeys.TEST_LABELS].roi
         roi_voxel = roi_array // self.spec[ArrayKeys.TEST_LABELS].voxel_size
 
         data = np.zeros(roi_voxel.get_shape(), dtype=np.uint32)
         data[:, ::2] = 100
 
-        for vertex in self.points:
-            loc = self.point_to_voxel(roi_array, vertex.location)
+        for vertex in self.vertices:
+            loc = self.vertex_to_voxel(roi_array, vertex.location)
             data[loc] = vertex.id
 
         spec = self.spec[ArrayKeys.TEST_LABELS].copy()
         spec.roi = roi_array
         batch.arrays[ArrayKeys.TEST_LABELS] = Array(data, spec=spec)
 
-        points = []
-        for vertex in self.points:
-            if roi_points.contains(vertex.location):
-                points.append(vertex)
-        batch.graphs[GraphKeys.TEST_POINTS] = Graph(
-            vertices=points, edges=[], spec=GraphSpec(roi=roi_points)
+        vertices = []
+        for vertex in self.vertices:
+            if roi_graph.contains(vertex.location):
+                vertices.append(vertex)
+        batch.graphs[GraphKeys.TEST_GRAPH] = Graph(
+            vertices=vertices, edges=[], spec=GraphSpec(roi=roi_graph)
         )
 
         return batch
@@ -93,11 +93,11 @@ class TestElasticAugment(ProviderTest):
     def test_3d_basics(self):
 
         test_labels = ArrayKey("TEST_LABELS")
-        test_points = GraphKey("TEST_POINTS")
+        test_graph = GraphKey("TEST_GRAPH")
         test_raster = ArrayKey("TEST_RASTER")
 
         pipeline = (
-            PointTestSource3D()
+            GraphTestSource3D()
             + ElasticAugment(
                 [10, 10, 10],
                 [0.1, 0.1, 0.1],
@@ -108,7 +108,7 @@ class TestElasticAugment(ProviderTest):
             # [math.pi/4, math.pi/4]) + # rotate by 45 deg
             # [0, 0]) + # no rotation
             RasterizePoints(
-                test_points,
+                test_graph,
                 test_raster,
                 settings=RasterizationSettings(radius=2, mode="peak"),
             )
@@ -128,27 +128,26 @@ class TestElasticAugment(ProviderTest):
 
                 request = BatchRequest()
                 request[test_labels] = ArraySpec(roi=request_roi)
-                request[test_points] = GraphSpec(roi=request_roi)
+                request[test_graph] = GraphSpec(roi=request_roi)
                 request[test_raster] = ArraySpec(roi=request_roi)
 
                 batch = pipeline.request_batch(request)
                 labels = batch[test_labels]
-                points = batch[test_points]
+                graph = batch[test_graph]
 
-                # the point at (0, 0, 0) should not have moved
-                print(list(points.vertices))
-                self.assertTrue(
-                    Vertex(id=0, location=np.array([0, 0, 0])) in list(points.vertices)
+                # the vertex at (0, 0, 0) should not have moved
+                self.assertIn(
+                    Vertex(id=0, location=np.array([0, 0, 0])), list(graph.vertices)
                 )
 
                 labels_data_roi = (
                     labels.spec.roi - labels.spec.roi.get_begin()
                 ) / labels.spec.voxel_size
 
-                # points should have moved together with the voxels
-                for i, point in points.data.items():
-                    loc = point.location - labels.spec.roi.get_begin()
+                # graph should have moved together with the voxels
+                for vertex in graph.vertices:
+                    loc = vertex.location - labels.spec.roi.get_begin()
                     loc = loc / labels.spec.voxel_size
                     loc = Coordinate(int(round(x)) for x in loc)
                     if labels_data_roi.contains(loc):
-                        self.assertEqual(labels.data[loc], i)
+                        self.assertEqual(labels.data[loc], vertex.id)
