47966e1c5f6cc6098f4c8234a169802ac994c0b8
Riley White <rileywhite89@gmail.com>
2020-08-07 11:19:51 -0400

    Add tests to random location graphs

--- tests/cases/random_location_graph.py
+++ tests/cases/random_location_graph.py
@@ -13,9 +13,13 @@ from gunpowder import (
     build,
 )
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
-class ExampleSourceRandomLocation(BatchProvider):
+class TestSourceRandomLocation(BatchProvider):
     def __init__(self):
 
         self.graph = Graph(
@@ -43,11 +47,155 @@ class ExampleSourceRandomLocation(BatchProvider):
 
 
 class TestRandomLocationGraph(ProviderTest):
-    def test_output(self):
+
+    def test_dim_size_1(self):
 
         GraphKey("TEST_GRAPH")
 
-        pipeline = ExampleSourceRandomLocation() + RandomLocation(
+        pipeline = TestSourceRandomLocation() + RandomLocation(
+            ensure_nonempty=GraphKeys.TEST_GRAPH
+        )
+
+        # count the number of times we get each node
+        histogram = {}
+
+        with build(pipeline):
+
+            for i in range(5000):
+                batch = pipeline.request_batch(
+                    BatchRequest(
+                        {
+                            GraphKeys.TEST_GRAPH: GraphSpec(
+                                roi=Roi((0, 0, 0), (1, 100, 100))
+                            )
+                        }
+                    )
+                )
+
+                nodes = list(batch[GraphKeys.TEST_GRAPH].nodes)
+                node_ids = [v.id for v in nodes]
+
+                self.assertTrue(len(nodes) > 0)
+                self.assertTrue(
+                    (1 in node_ids) != (2 in node_ids or 3 in node_ids),
+                    node_ids,
+                )
+
+                for node in batch[GraphKeys.TEST_GRAPH].nodes:
+                    if node.id not in histogram:
+                        histogram[node.id] = 1
+                    else:
+                        histogram[node.id] += 1
+
+        total = sum(histogram.values())
+        for k, v in histogram.items():
+            histogram[k] = float(v) / total
+
+        # we should get roughly the same count for each point
+        for i in histogram.keys():
+            for j in histogram.keys():
+                self.assertAlmostEqual(histogram[i], histogram[j], 1)
+
+    def test_req_full_roi(self):
+
+        GraphKey("TEST_GRAPH")
+
+        pipeline = TestSourceRandomLocation() + RandomLocation(
+            ensure_nonempty=GraphKeys.TEST_GRAPH
+        )
+
+        # count the number of times we get each node
+        histogram = {}
+
+        with build(pipeline):
+
+            for i in range(5000):
+                batch = pipeline.request_batch(
+                    BatchRequest(
+                        {
+                            GraphKeys.TEST_GRAPH: GraphSpec(
+                                roi=Roi((0, 0, 0), (1000, 1000, 1000))
+                            )
+                        }
+                    )
+                )
+
+                nodes = list(batch[GraphKeys.TEST_GRAPH].nodes)
+                node_ids = [v.id for v in nodes]
+
+                self.assertTrue(len(nodes) > 0)
+                self.assertTrue(
+                    len(node_ids) == 3,
+                    node_ids,
+                )
+
+                for node in batch[GraphKeys.TEST_GRAPH].nodes:
+                    if node.id not in histogram:
+                        histogram[node.id] = 1
+                    else:
+                        histogram[node.id] += 1
+
+        total = sum(histogram.values())
+        for k, v in histogram.items():
+            histogram[k] = float(v) / total
+
+        # we should get roughly the same count for each point
+        for i in histogram.keys():
+            for j in histogram.keys():
+                self.assertAlmostEqual(histogram[i], histogram[j], 1)
+
+    def test_roi_one_point(self):
+
+        GraphKey("TEST_GRAPH")
+
+        pipeline = TestSourceRandomLocation() + RandomLocation(
+            ensure_nonempty=GraphKeys.TEST_GRAPH
+        )
+
+        # count the number of times we get each node
+        histogram = {}
+
+        with build(pipeline):
+
+            for i in range(5000):
+                batch = pipeline.request_batch(
+                    BatchRequest(
+                        {
+                            GraphKeys.TEST_GRAPH: GraphSpec(
+                                roi=Roi((0, 0, 0), (1, 1, 1))
+                            )
+                        }
+                    )
+                )
+
+                nodes = list(batch[GraphKeys.TEST_GRAPH].nodes)
+                node_ids = [v.id for v in nodes]
+
+                self.assertTrue(len(nodes) > 0)
+                self.assertTrue(
+                    (1 in node_ids) != (2 in node_ids or 3 in node_ids),
+                    node_ids,
+                )
+
+                for node in batch[GraphKeys.TEST_GRAPH].nodes:
+                    if node.id not in histogram:
+                        histogram[node.id] = 1
+                    else:
+                        histogram[node.id] += 1
+
+        total = sum(histogram.values())
+        for k, v in histogram.items():
+            histogram[k] = float(v) / total
+
+        # we should get roughly the same count for each point
+        for i in histogram.keys():
+            for j in histogram.keys():
+                self.assertAlmostEqual(histogram[i], histogram[j], 1)
+    def test_iso_roi(self):
+
+        GraphKey("TEST_GRAPH")
+
+        pipeline = TestSourceRandomLocation() + RandomLocation(
             ensure_nonempty=GraphKeys.TEST_GRAPH
         )
 
