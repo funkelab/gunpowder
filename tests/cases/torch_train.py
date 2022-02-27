e90c4a80982fdd0ca948924c32f6f3b2f1a4a8fb
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2022-02-27 16:55:37 -0500

    Restore torch tests

--- tests/cases/torch_train.py
+++ tests/cases/torch_train.py
@@ -88,143 +88,143 @@ class ExampleTorchTrainSource(BatchProvider):
         return batch
 
 
-# @skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
-# class TestTorchTrain(ProviderTest):
-#     def test_output(self):
+@skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
+class TestTorchTrain(ProviderTest):
+    def test_output(self):
 
-#         logging.getLogger("gunpowder.torch.nodes.train").setLevel(logging.INFO)
+        logging.getLogger("gunpowder.torch.nodes.train").setLevel(logging.INFO)
 
-#         checkpoint_basename = self.path_to("model")
+        checkpoint_basename = self.path_to("model")
 
-#         ArrayKey("A")
-#         ArrayKey("B")
-#         ArrayKey("C")
-#         ArrayKey("C_PREDICTED")
-#         ArrayKey("C_GRADIENT")
+        ArrayKey("A")
+        ArrayKey("B")
+        ArrayKey("C")
+        ArrayKey("C_PREDICTED")
+        ArrayKey("C_GRADIENT")
 
-#         class ExampleModel(torch.nn.Module):
-#             def __init__(self):
-#                 super(ExampleModel, self).__init__()
-#                 self.linear = torch.nn.Linear(4, 1, False)
+        class ExampleModel(torch.nn.Module):
+            def __init__(self):
+                super(ExampleModel, self).__init__()
+                self.linear = torch.nn.Linear(4, 1, False)
 
-#             def forward(self, a, b):
-#                 a = a.reshape(-1)
-#                 b = b.reshape(-1)
-#                 return self.linear(a * b)
+            def forward(self, a, b):
+                a = a.reshape(-1)
+                b = b.reshape(-1)
+                return self.linear(a * b)
 
-#         model = ExampleModel()
-#         loss = torch.nn.MSELoss()
-#         optimizer = torch.optim.SGD(model.parameters(), lr=1e-7, momentum=0.999)
+        model = ExampleModel()
+        loss = torch.nn.MSELoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-7, momentum=0.999)
 
-#         source = ExampleTorchTrainSource()
-#         train = Train(
-#             model=model,
-#             optimizer=optimizer,
-#             loss=loss,
-#             inputs={"a": ArrayKeys.A, "b": ArrayKeys.B},
-#             loss_inputs={0: ArrayKeys.C_PREDICTED, 1: ArrayKeys.C},
-#             outputs={0: ArrayKeys.C_PREDICTED},
-#             gradients={0: ArrayKeys.C_GRADIENT},
-#             array_specs={
-#                 ArrayKeys.C_PREDICTED: ArraySpec(nonspatial=True),
-#                 ArrayKeys.C_GRADIENT: ArraySpec(nonspatial=True),
-#             },
-#             checkpoint_basename=checkpoint_basename,
-#             save_every=100,
-#             spawn_subprocess=True,
-#         )
-#         pipeline = source + train
+        source = ExampleTorchTrainSource()
+        train = Train(
+            model=model,
+            optimizer=optimizer,
+            loss=loss,
+            inputs={"a": ArrayKeys.A, "b": ArrayKeys.B},
+            loss_inputs={0: ArrayKeys.C_PREDICTED, 1: ArrayKeys.C},
+            outputs={0: ArrayKeys.C_PREDICTED},
+            gradients={0: ArrayKeys.C_GRADIENT},
+            array_specs={
+                ArrayKeys.C_PREDICTED: ArraySpec(nonspatial=True),
+                ArrayKeys.C_GRADIENT: ArraySpec(nonspatial=True),
+            },
+            checkpoint_basename=checkpoint_basename,
+            save_every=100,
+            spawn_subprocess=True,
+        )
+        pipeline = source + train
 
-#         request = BatchRequest(
-#             {
-#                 ArrayKeys.A: ArraySpec(roi=Roi((0, 0), (2, 2))),
-#                 ArrayKeys.B: ArraySpec(roi=Roi((0, 0), (2, 2))),
-#                 ArrayKeys.C: ArraySpec(nonspatial=True),
-#                 ArrayKeys.C_PREDICTED: ArraySpec(nonspatial=True),
-#                 ArrayKeys.C_GRADIENT: ArraySpec(nonspatial=True),
-#             }
-#         )
+        request = BatchRequest(
+            {
+                ArrayKeys.A: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                ArrayKeys.B: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                ArrayKeys.C: ArraySpec(nonspatial=True),
+                ArrayKeys.C_PREDICTED: ArraySpec(nonspatial=True),
+                ArrayKeys.C_GRADIENT: ArraySpec(nonspatial=True),
+            }
+        )
 
-#         # train for a couple of iterations
-#         with build(pipeline):
+        # train for a couple of iterations
+        with build(pipeline):
 
-#             batch = pipeline.request_batch(request)
+            batch = pipeline.request_batch(request)
 
-#             for i in range(200 - 1):
-#                 loss1 = batch.loss
-#                 batch = pipeline.request_batch(request)
-#                 loss2 = batch.loss
-#                 self.assertLess(loss2, loss1)
+            for i in range(200 - 1):
+                loss1 = batch.loss
+                batch = pipeline.request_batch(request)
+                loss2 = batch.loss
+                self.assertLess(loss2, loss1)
 
-#         # resume training
-#         with build(pipeline):
+        # resume training
+        with build(pipeline):
 
-#             for i in range(100):
-#                 loss1 = batch.loss
-#                 batch = pipeline.request_batch(request)
-#                 loss2 = batch.loss
-#                 self.assertLess(loss2, loss1)
+            for i in range(100):
+                loss1 = batch.loss
+                batch = pipeline.request_batch(request)
+                loss2 = batch.loss
+                self.assertLess(loss2, loss1)
 
 
-# @skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
-# class TestTorchPredict(ProviderTest):
-#     def test_output(self):
-#         logging.getLogger("gunpowder.torch.nodes.predict").setLevel(logging.INFO)
+@skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
+class TestTorchPredict(ProviderTest):
+    def test_output(self):
+        logging.getLogger("gunpowder.torch.nodes.predict").setLevel(logging.INFO)
 
-#         a = ArrayKey("A")
-#         b = ArrayKey("B")
-#         c = ArrayKey("C")
-#         c_pred = ArrayKey("C_PREDICTED")
-#         d_pred = ArrayKey("D_PREDICTED")
+        a = ArrayKey("A")
+        b = ArrayKey("B")
+        c = ArrayKey("C")
+        c_pred = ArrayKey("C_PREDICTED")
+        d_pred = ArrayKey("D_PREDICTED")
 
-#         class ExampleModel(torch.nn.Module):
-#             def __init__(self):
-#                 super(ExampleModel, self).__init__()
-#                 self.linear = torch.nn.Linear(4, 1, False)
-#                 self.linear.weight.data = torch.Tensor([1, 1, 1, 1])
+        class ExampleModel(torch.nn.Module):
+            def __init__(self):
+                super(ExampleModel, self).__init__()
+                self.linear = torch.nn.Linear(4, 1, False)
+                self.linear.weight.data = torch.Tensor([1, 1, 1, 1])
 
-#             def forward(self, a, b):
-#                 a = a.reshape(-1)
-#                 b = b.reshape(-1)
-#                 c_pred = self.linear(a * b)
-#                 d_pred = c_pred * 2
-#                 return d_pred
+            def forward(self, a, b):
+                a = a.reshape(-1)
+                b = b.reshape(-1)
+                c_pred = self.linear(a * b)
+                d_pred = c_pred * 2
+                return d_pred
 
-#         model = ExampleModel()
+        model = ExampleModel()
 
-#         source = ExampleTorchTrainSource()
-#         predict = Predict(
-#             model=model,
-#             inputs={"a": a, "b": b},
-#             outputs={"linear": c_pred, 0: d_pred},
-#             array_specs={
-#                 c: ArraySpec(nonspatial=True),
-#                 c_pred: ArraySpec(nonspatial=True),
-#                 d_pred: ArraySpec(nonspatial=True),
-#             },
-#             spawn_subprocess=True,
-#         )
-#         pipeline = source + predict
+        source = ExampleTorchTrainSource()
+        predict = Predict(
+            model=model,
+            inputs={"a": a, "b": b},
+            outputs={"linear": c_pred, 0: d_pred},
+            array_specs={
+                c: ArraySpec(nonspatial=True),
+                c_pred: ArraySpec(nonspatial=True),
+                d_pred: ArraySpec(nonspatial=True),
+            },
+            spawn_subprocess=True,
+        )
+        pipeline = source + predict
 
-#         request = BatchRequest(
-#             {
-#                 a: ArraySpec(roi=Roi((0, 0), (2, 2))),
-#                 b: ArraySpec(roi=Roi((0, 0), (2, 2))),
-#                 c: ArraySpec(nonspatial=True),
-#                 c_pred: ArraySpec(nonspatial=True),
-#                 d_pred: ArraySpec(nonspatial=True),
-#             }
-#         )
+        request = BatchRequest(
+            {
+                a: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                b: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                c: ArraySpec(nonspatial=True),
+                c_pred: ArraySpec(nonspatial=True),
+                d_pred: ArraySpec(nonspatial=True),
+            }
+        )
 
-#         # train for a couple of iterations
-#         with build(pipeline):
+        # train for a couple of iterations
+        with build(pipeline):
 
-#             batch1 = pipeline.request_batch(request)
-#             batch2 = pipeline.request_batch(request)
+            batch1 = pipeline.request_batch(request)
+            batch2 = pipeline.request_batch(request)
 
-#             assert np.isclose(batch1[c_pred].data, batch2[c_pred].data)
-#             assert np.isclose(batch1[c_pred].data, 1 + 4 + 9)
-#             assert np.isclose(batch2[d_pred].data, 2 * (1 + 4 + 9))
+            assert np.isclose(batch1[c_pred].data, batch2[c_pred].data)
+            assert np.isclose(batch1[c_pred].data, 1 + 4 + 9)
+            assert np.isclose(batch2[d_pred].data, 2 * (1 + 4 + 9))
 
 
 class ExampleModel(torch.nn.Module):
@@ -277,37 +277,37 @@ class TestTorchPredictMultiprocessing(ProviderTest):
             batch = pipeline.request_batch(request)
             assert pred in batch
 
-    # def test_precache(self):
+    def test_precache(self):
 
-    #     logging.getLogger("gunpowder.torch.nodes.predict").setLevel(logging.INFO)
+        logging.getLogger("gunpowder.torch.nodes.predict").setLevel(logging.INFO)
 
-    #     a = ArrayKey("A")
-    #     pred = ArrayKey("PRED")
+        a = ArrayKey("A")
+        pred = ArrayKey("PRED")
 
-    #     model = ExampleModel()
+        model = ExampleModel()
 
-    #     reference_request = BatchRequest()
-    #     reference_request[a] = ArraySpec(roi=Roi((0, 0), (7, 7)))
-    #     reference_request[pred] = ArraySpec(roi=Roi((1, 1), (5, 5)))
+        reference_request = BatchRequest()
+        reference_request[a] = ArraySpec(roi=Roi((0, 0), (7, 7)))
+        reference_request[pred] = ArraySpec(roi=Roi((1, 1), (5, 5)))
 
-    #     source = ExampleTorchTrain2DSource()
-    #     predict = Predict(
-    #         model=model,
-    #         inputs={"a": a},
-    #         outputs={0: pred},
-    #         array_specs={pred: ArraySpec()},
-    #     )
-    #     pipeline = source + predict + PreCache(cache_size=3, num_workers=2)
+        source = ExampleTorchTrain2DSource()
+        predict = Predict(
+            model=model,
+            inputs={"a": a},
+            outputs={0: pred},
+            array_specs={pred: ArraySpec()},
+        )
+        pipeline = source + predict + PreCache(cache_size=3, num_workers=2)
 
-    #     request = BatchRequest(
-    #         {
-    #             a: ArraySpec(roi=Roi((0, 0), (17, 17))),
-    #             pred: ArraySpec(roi=Roi((0, 0), (15, 15))),
-    #         }
-    #     )
+        request = BatchRequest(
+            {
+                a: ArraySpec(roi=Roi((0, 0), (17, 17))),
+                pred: ArraySpec(roi=Roi((0, 0), (15, 15))),
+            }
+        )
 
-    #     # train for a couple of iterations
-    #     with build(pipeline):
+        # train for a couple of iterations
+        with build(pipeline):
 
-    #         batch = pipeline.request_batch(request)
-    #         assert pred in batch
+            batch = pipeline.request_batch(request)
+            assert pred in batch
