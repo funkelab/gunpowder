c0426200f6c4e772c777bb53a577c0ec0597eded
pattonw <pattonw@hhmi.org>
2022-10-19 16:07:24 -0700

    standardize upsample test

--- tests/cases/upsample.py
+++ tests/cases/upsample.py
@@ -2,6 +2,7 @@ from .helper_sources import ArraySource
 from gunpowder import *
 import numpy as np
 
+
 def test_output():
 
     raw = ArrayKey("RAW")
@@ -10,18 +11,20 @@ def test_output():
     gt_upsampled = ArrayKey("GT_LABELS_UPSAMPLED")
 
     request = BatchRequest()
-    request.add(raw, (200,200,200))
-    request.add(raw_upsampled, (120,120,120))
-    request.add(gt, (200,200,200))
-    request.add(gt_upsampled, (200,200,200))
+    request.add(raw, (200, 200, 200))
+    request.add(raw_upsampled, (120, 120, 120))
+    request.add(gt, (200, 200, 200))
+    request.add(gt_upsampled, (200, 200, 200))
 
-    meshgrids = np.meshgrid(range(0, 250), range(0, 250), range(0, 250), indexing="ij")
+    meshgrids = np.meshgrid(
+        range(0, 1000, 4), range(0, 1000, 4), range(0, 1000, 4), indexing="ij"
+    )
     data = meshgrids[0] + meshgrids[1] + meshgrids[2]
 
     raw_source = ArraySource(
         raw,
         Array(
-            np.stack([data, data]),
+            data,
             ArraySpec(roi=Roi((0, 0, 0), (1000, 1000, 1000)), voxel_size=(4, 4, 4)),
         ),
     )
@@ -34,9 +37,10 @@ def test_output():
     )
 
     pipeline = (
-            (raw_source, gt_source) + MergeProvider() +
-            UpSample(raw, 2, raw_upsampled) +
-            UpSample(gt, 2, gt_upsampled)
+        (raw_source, gt_source)
+        + MergeProvider()
+        + UpSample(raw, 2, raw_upsampled)
+        + UpSample(gt, 2, gt_upsampled)
     )
 
     with build(pipeline):
@@ -44,35 +48,39 @@ def test_output():
 
     for (array_key, array) in batch.arrays.items():
 
-        # assert that pixels encode their position for supposedly unaltered 
+        # assert that pixels encode their position for supposedly unaltered
         # arrays
         if array_key in [raw, gt]:
 
             # the z,y,x coordinates of the ROI
             meshgrids = np.meshgrid(
-                    range(roi.begin[0], roi.end[0], 4),
-                    range(roi.begin[1], roi.end[1], 4),
-                    range(roi.begin[2], roi.end[2], 4), indexing='ij')
+                range(array.spec.roi.begin[0], array.spec.roi.end[0], 4),
+                range(array.spec.roi.begin[1], array.spec.roi.end[1], 4),
+                range(array.spec.roi.begin[2], array.spec.roi.end[2], 4),
+                indexing="ij",
+            )
             data = meshgrids[0] + meshgrids[1] + meshgrids[2]
 
             if array_key == raw:
-                assert np.array_equal(array.data[0], data) and np.array_equal(array.data[1], data)
+                assert np.array_equal(array.data, data) and np.array_equal(
+                    array.data, data
+                ), f"{array.data, data}"
             else:
                 assert np.array_equal(array.data, data), str(array_key)
 
         elif array_key == raw_upsampled:
 
-            assert array.data[0,0,0] == 30
-            assert array.data[1,0,0] == 30
-            assert array.data[2,0,0] == 31
-            assert array.data[3,0,0] == 31
+            assert array.data[0, 0, 0] == 120
+            assert array.data[1, 0, 0] == 120
+            assert array.data[2, 0, 0] == 124
+            assert array.data[3, 0, 0] == 124
 
         elif array_key == gt_upsampled:
 
-            assert array.data[0,0,0] == 0
-            assert array.data[1,0,0] == 0
-            assert array.data[2,0,0] == 1
-            assert array.data[3,0,0] == 1
+            assert array.data[0, 0, 0] == 0
+            assert array.data[1, 0, 0] == 0
+            assert array.data[2, 0, 0] == 4
+            assert array.data[3, 0, 0] == 4
 
         else:
 
