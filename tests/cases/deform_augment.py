ff0ecbc0c3fdc7c75dc7d0d37f21642f98df8b9c
pattonw <pattonw@hhmi.org>
2023-04-05 15:37:09 +0000

    add multi-resolution to deform augment test

--- tests/cases/deform_augment.py
+++ tests/cases/deform_augment.py
@@ -23,12 +23,12 @@ import math
 
 
 class GraphTestSource3D(BatchProvider):
-    def __init__(self, graph_key: GraphKey, array_key: ArrayKey):
+    def __init__(self, graph_key: GraphKey, array_key: ArrayKey, array_key2: ArrayKey):
         self.graph_key = graph_key
         self.array_key = array_key
+        self.array_key2 = array_key2
 
     def setup(self):
-
         self.nodes = [
             Node(id=1, location=np.array([0, 0, 0])),
             Node(id=2, location=np.array([0, 10, 0])),
@@ -52,34 +52,65 @@ class GraphTestSource3D(BatchProvider):
             ),
         )
 
-    def node_to_voxel(self, array_roi, location):
+        self.provides(
+            self.array_key2,
+            ArraySpec(
+                roi=Roi((-100, -100, -100), (200, 200, 200)),
+                voxel_size=Coordinate((1, 2, 1)),
+                interpolatable=False,
+            ),
+        )
 
+    def node_to_voxel(self, array_roi, voxel_size, location):
         # location is in world units, get it into voxels
-        location = location / self.spec[self.array_key].voxel_size
+        location = location / voxel_size
 
         # shift location relative to beginning of array roi
-        location -= array_roi.begin / self.spec[self.array_key].voxel_size
+        location -= array_roi.begin / voxel_size
 
-        return tuple(slice(int(l - 2), int(l + 3)) for l in location)
+        return tuple(slice(int(l - 1), int(l + 2)) for l in location)
 
     def provide(self, request):
-
         batch = Batch()
 
         roi_graph = request[self.graph_key].roi
-        roi_array = request[self.array_key].roi
-        roi_voxel = roi_array // self.spec[self.array_key].voxel_size
+        roi_voxel = request[self.array_key].roi // self.spec[self.array_key].voxel_size
 
-        data = np.zeros(roi_voxel.shape, dtype=np.uint32)
+        data = np.zeros(
+            (request[self.array_key].roi // self.spec[self.array_key].voxel_size).shape,
+            dtype=np.uint32,
+        )
 
         for node in self.nodes:
-            loc = self.node_to_voxel(roi_array, node.location)
-            print(node, roi_array, node.location, loc)
+            loc = self.node_to_voxel(
+                request[self.array_key].roi,
+                self.spec[self.array_key].voxel_size,
+                node.location,
+            )
             data[loc] = node.id
 
+        data2 = np.zeros(
+            (
+                request[self.array_key2].roi // self.spec[self.array_key2].voxel_size
+            ).shape,
+            dtype=np.uint32,
+        )
+
+        for node in self.nodes:
+            loc = self.node_to_voxel(
+                request[self.array_key2].roi,
+                self.spec[self.array_key2].voxel_size,
+                node.location,
+            )
+            data2[loc] = node.id
+
         spec = self.spec[self.array_key].copy()
-        spec.roi = roi_array
+        spec.roi = request[self.array_key].roi
         batch.arrays[self.array_key] = Array(data, spec=spec)
+
+        spec2 = self.spec[self.array_key2].copy()
+        spec2.roi = request[self.array_key2].roi
+        batch.arrays[self.array_key2] = Array(data2, spec=spec2)
 
         nodes = []
         for node in self.nodes:
@@ -93,55 +124,41 @@ class GraphTestSource3D(BatchProvider):
 
 
 def test_3d_basics():
-
     test_labels = ArrayKey("TEST_LABELS")
+    test_labels2 = ArrayKey("TEST_LABELS2")
     test_graph = GraphKey("TEST_GRAPH")
-    test_raster = ArrayKey("TEST_RASTER")
 
-    pipeline = (
-        GraphTestSource3D(test_graph, test_labels)
-        + DeformAugment(
-            [10, 10, 10],
-            [0.1, 0.1, 0.1],
-        )
-        + RasterizeGraph(
-            test_graph,
-            test_raster,
-            settings=RasterizationSettings(radius=2, mode="peak"),
-        )
+    pipeline = GraphTestSource3D(test_graph, test_labels, test_labels2) + DeformAugment(
+        [10, 10, 10], [2, 2, 2], graph_raster_voxel_size=(2, 2, 2)
     )
 
     for _ in range(5):
-
         with build(pipeline):
-
             request_roi = Roi((-20, -20, -20), (40, 40, 40))
 
             request = BatchRequest()
             request[test_labels] = ArraySpec(roi=request_roi)
+            request[test_labels2] = ArraySpec(roi=request_roi / 2)
             request[test_graph] = GraphSpec(roi=request_roi)
-            request[test_raster] = ArraySpec(roi=request_roi)
 
             batch = pipeline.request_batch(request)
             labels = batch[test_labels]
+            labels2 = batch[test_labels2]
             graph = batch[test_graph]
-            raster = batch[test_raster]
 
             assert Node(id=1, location=np.array([0, 0, 0])) in list(graph.nodes)
 
-            labels_data_roi = (
-                labels.spec.roi - labels.spec.roi.begin
-            ) / labels.spec.voxel_size
-
             # graph should have moved together with the voxels
             for node in graph.nodes:
+
                 loc = node.location - labels.spec.roi.begin
-                loc = loc / labels.spec.voxel_size
-                loc = Coordinate(int(round(x)) for x in loc)
-                print(node)
-                print(loc)
-                print(np.unique(labels.data))
-                print(np.argwhere(labels.data==node.id))
-                if labels_data_roi.contains(loc):
+                if labels.spec.roi.contains(loc):
+                    loc = loc / labels.spec.voxel_size
+                    loc = Coordinate(int(round(x)) for x in loc)
                     assert labels.data[loc] == node.id
-                    assert raster.data[loc] == node.id
+
+                loc2 = node.location - labels2.spec.roi.begin
+                if labels2.spec.roi.contains(loc2):
+                    loc2 = loc2 / labels2.spec.voxel_size
+                    loc2 = Coordinate(int(round(x)) for x in loc2)
+                    assert labels2.data[loc2] == node.id
