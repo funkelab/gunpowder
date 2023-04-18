9b659048d7315deb1ab9b27f1dd7afca0161aba0
pattonw <pattonw@hhmi.org>
2023-04-17 21:27:31 -0400

    fix intensity_scale_shift tests

--- tests/cases/intensity_scale_shift.py
+++ tests/cases/intensity_scale_shift.py
@@ -15,21 +15,21 @@ import numpy as np
 
 
 def test_shift():
-
     raw_key = ArrayKey("RAW")
     array = Array(
-        np.ones((10, 10)) * 2, ArraySpec(Roi((0, 0), (10, 10)), Coordinate(1, 1))
+        np.ones((10, 10), dtype=np.float32) * 2,
+        ArraySpec(Roi((0, 0), (10, 10)), Coordinate(1, 1)),
     )
     source = ArraySource(raw_key, array)
 
     request = BatchRequest()
     request.add(raw_key, Coordinate(10, 10))
 
-    pipeline = source + Normalize(raw_key) + IntensityScaleShift(raw_key, 0.5, 10)
+    pipeline = source + IntensityScaleShift(raw_key, 0.5, 10)
 
     with build(pipeline):
         batch = pipeline.request_batch(request)
 
         x = batch.arrays[raw_key].data
-        assert np.isclose(x.min(), 10)
-        assert np.isclose(x.max(), 10)
+        assert np.isclose(x.min(), 11)
+        assert np.isclose(x.max(), 11)
