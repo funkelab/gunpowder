412d46f5ee46c22afa6b9f0990dea1d012555861
pattonw <pattonw@hhmi.org>
2023-04-17 21:36:23 -0400

    Add expected failure test
    To work on error messages it is nice to have some examples of errors.

---
+++ tests/cases/expected_failures.py
@@ -0,0 +1,37 @@
+import gunpowder as gp
+from gunpowder.nodes.batch_provider import BatchRequestError
+from .helper_sources import ArraySource
+
+from funlib.geometry import Roi, Coordinate
+
+import numpy as np
+import pytest
+
+
+@pytest.mark.xfail()
+def test_request_too_large():
+    raw_key = gp.ArrayKey("RAW")
+    data = np.ones((10, 10))
+    array = gp.Array(
+        data, gp.ArraySpec(voxel_size=Coordinate(100, 100), interpolatable=True)
+    )
+    source = ArraySource(raw_key, array)
+
+    pipeline = (
+        source
+        + gp.Normalize(raw_key)
+        + gp.RandomLocation()
+        + gp.IntensityAugment(
+            raw_key, scale_min=0, scale_max=0, shift_min=0.5, shift_max=0.5
+        )
+    )
+
+    request = gp.BatchRequest()
+    request.add(raw_key, (10000, 10000))
+
+    with gp.build(pipeline):
+        for i in range(100):
+            with pytest.raises(BatchRequestError):
+                batch = pipeline.request_batch(request)
+
+            x = batch.arrays[raw_key].data
