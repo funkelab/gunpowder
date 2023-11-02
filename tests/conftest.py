96ff2f13c34b0aa6fba9d96d5ccea93d625882e4
William Patton <wllmpttn24@gmail.com>
2023-11-01 22:06:40 -0700

    remove multiprocess set start method monkey patch
    We want to test with both fork and spawn start methods, but this
    seems to interfere with the torch tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@ import multiprocessing as mp
 # cannot parametrize unittest.TestCase. We should test both
 # fork and spawn but I'm not sure how to.
 # @pytest.fixture(params=["fork", "spawn"], autouse=True)
-@pytest.fixture(autouse=True)
-def context(monkeypatch):
-    ctx = mp.get_context("spawn")
-    monkeypatch.setattr(mp, "Queue", ctx.Queue)
-    monkeypatch.setattr(mp, "Process", ctx.Process)
-    monkeypatch.setattr(mp, "Event", ctx.Event)
-    monkeypatch.setattr(mp, "Value", ctx.Value)
+# @pytest.fixture(autouse=True)
+# def context(monkeypatch):
+#     ctx = mp.get_context("spawn")
+#     monkeypatch.setattr(mp, "Queue", ctx.Queue)
+#     monkeypatch.setattr(mp, "Process", ctx.Process)
+#     monkeypatch.setattr(mp, "Event", ctx.Event)
+#     monkeypatch.setattr(mp, "Value", ctx.Value)
