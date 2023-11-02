704f75a92658336c752bf45d3ea30f7fa4e68e3d
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2023-11-02 16:43:20 +0000

    tests for device arg passed
    # Issue #188

--- tests/cases/torch_train.py
+++ tests/cases/torch_train.py
@@ -161,6 +161,82 @@ class TestTorchTrain(ProviderTest):
 
 
 @skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
+class TestTorchTrainCudaDevice(ProviderTest):
+    def test_output(self):
+        logging.getLogger("gunpowder.torch.nodes.train").setLevel(logging.INFO)
+
+        checkpoint_basename = self.path_to("model")
+
+        ArrayKey("A")
+        ArrayKey("B")
+        ArrayKey("C")
+        ArrayKey("C_PREDICTED")
+        ArrayKey("C_GRADIENT")
+
+        class ExampleModel(torch.nn.Module):
+            def __init__(self):
+                super(ExampleModel, self).__init__()
+                self.linear = torch.nn.Linear(4, 1, False)
+
+            def forward(self, a, b):
+                a = a.reshape(-1)
+                b = b.reshape(-1)
+                return self.linear(a * b)
+
+        model = ExampleModel()
+        loss = torch.nn.MSELoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-7, momentum=0.999)
+
+        source = ExampleTorchTrainSource()
+        train = Train(
+            model=model,
+            optimizer=optimizer,
+            loss=loss,
+            inputs={"a": ArrayKeys.A, "b": ArrayKeys.B},
+            loss_inputs={0: ArrayKeys.C_PREDICTED, 1: ArrayKeys.C},
+            outputs={0: ArrayKeys.C_PREDICTED},
+            gradients={0: ArrayKeys.C_GRADIENT},
+            array_specs={
+                ArrayKeys.C_PREDICTED: ArraySpec(nonspatial=True),
+                ArrayKeys.C_GRADIENT: ArraySpec(nonspatial=True),
+            },
+            checkpoint_basename=checkpoint_basename,
+            save_every=100,
+            spawn_subprocess=True,
+            device='cuda:0'
+        )
+        pipeline = source + train
+
+        request = BatchRequest(
+            {
+                ArrayKeys.A: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                ArrayKeys.B: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                ArrayKeys.C: ArraySpec(nonspatial=True),
+                ArrayKeys.C_PREDICTED: ArraySpec(nonspatial=True),
+                ArrayKeys.C_GRADIENT: ArraySpec(nonspatial=True),
+            }
+        )
+
+        # train for a couple of iterations
+        with build(pipeline):
+            batch = pipeline.request_batch(request)
+
+            for i in range(200 - 1):
+                loss1 = batch.loss
+                batch = pipeline.request_batch(request)
+                loss2 = batch.loss
+                self.assertLess(loss2, loss1)
+
+        # resume training
+        with build(pipeline):
+            for i in range(100):
+                loss1 = batch.loss
+                batch = pipeline.request_batch(request)
+                loss2 = batch.loss
+                self.assertLess(loss2, loss1)
+
+
+@skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
 class TestTorchPredict(ProviderTest):
     def test_output(self):
         logging.getLogger("gunpowder.torch.nodes.predict").setLevel(logging.INFO)
@@ -222,6 +298,81 @@ class TestTorchPredict(ProviderTest):
 
 if not isinstance(torch, NoSuchModule):
 
+    class ExampleModel(torch.nn.Module):
+        def __init__(self):
+            super(ExampleModel, self).__init__()
+            self.linear = torch.nn.Conv2d(1, 1, 3)
+
+        def forward(self, a):
+            a = a.unsqueeze(0).unsqueeze(0)
+            pred = self.linear(a)
+            a = a.squeeze(0).squeeze(0)
+            pred = pred.squeeze(0).squeeze(0)
+            return pred
+
+
+@skipIf(isinstance(torch, NoSuchModule), "torch is not installed")
+class TestTorchPredictCudaDevice(ProviderTest):
+    def test_output(self):
+        logging.getLogger("gunpowder.torch.nodes.predict").setLevel(logging.INFO)
+
+        a = ArrayKey("A")
+        b = ArrayKey("B")
+        c = ArrayKey("C")
+        c_pred = ArrayKey("C_PREDICTED")
+        d_pred = ArrayKey("D_PREDICTED")
+
+        class ExampleModel(torch.nn.Module):
+            def __init__(self):
+                super(ExampleModel, self).__init__()
+                self.linear = torch.nn.Linear(4, 1, False)
+                self.linear.weight.data = torch.Tensor([1, 1, 1, 1])
+
+            def forward(self, a, b):
+                a = a.reshape(-1)
+                b = b.reshape(-1)
+                c_pred = self.linear(a * b)
+                d_pred = c_pred * 2
+                return d_pred
+
+        model = ExampleModel()
+
+        source = ExampleTorchTrainSource()
+        predict = Predict(
+            model=model,
+            inputs={"a": a, "b": b},
+            outputs={"linear": c_pred, 0: d_pred},
+            array_specs={
+                c: ArraySpec(nonspatial=True),
+                c_pred: ArraySpec(nonspatial=True),
+                d_pred: ArraySpec(nonspatial=True),
+            },
+            spawn_subprocess=True,
+            device="cuda:0"
+        )
+        pipeline = source + predict
+
+        request = BatchRequest(
+            {
+                a: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                b: ArraySpec(roi=Roi((0, 0), (2, 2))),
+                c: ArraySpec(nonspatial=True),
+                c_pred: ArraySpec(nonspatial=True),
+                d_pred: ArraySpec(nonspatial=True),
+            }
+        )
+
+        # train for a couple of iterations
+        with build(pipeline):
+            batch1 = pipeline.request_batch(request)
+            batch2 = pipeline.request_batch(request)
+
+            assert np.isclose(batch1[c_pred].data, batch2[c_pred].data)
+            assert np.isclose(batch1[c_pred].data, 1 + 4 + 9)
+            assert np.isclose(batch2[d_pred].data, 2 * (1 + 4 + 9))
+
+
+if not isinstance(torch, NoSuchModule):
     class ExampleModel(torch.nn.Module):
         def __init__(self):
             super(ExampleModel, self).__init__()
