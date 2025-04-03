7d34a07f58cd671c72e0222be5565f723784b869
William Patton <wllmpttn24@gmail.com>
2025-04-03 16:50:40 +0700

    update deform test to use a simplified fixture instead of a custom node, also test rotation axes

--- tests/cases/deform_augment.py
+++ tests/cases/deform_augment.py
@@ -1,9 +1,10 @@
 import numpy as np
+import dask.array as da
 import pytest
 from scipy.ndimage import center_of_mass
+from funlib.persistence import Array
 
 from gunpowder import (
-    Array,
     ArrayKey,
     ArraySpec,
     Batch,
@@ -12,125 +13,83 @@ from gunpowder import (
     Coordinate,
     DeformAugment,
     GPGraphSource,
+    ArraySource,
     GraphKey,
     GraphSpec,
     Pipeline,
     Roi,
     build,
+    MergeProvider,
 )
 from gunpowder.graph import Graph, Node
 
 
-class GraphTestSource3D(BatchProvider):
-    def __init__(self, graph_key: GraphKey, array_key: ArrayKey, array_key2: ArrayKey):
-        self.graph_key = graph_key
-        self.array_key = array_key
-        self.array_key2 = array_key2
-
-    def setup(self):
-        self.nodes = [
-            Node(id=1, location=np.array([0, 0.5, 0])),
-            Node(id=2, location=np.array([0, 10.5, 0])),
-            Node(id=3, location=np.array([0, 20.5, 0])),
-            Node(id=4, location=np.array([0, 30.5, 0])),
-            Node(id=5, location=np.array([0, 40.5, 0])),
-            Node(id=6, location=np.array([0, 50.5, 0])),
-        ]
-
-        self.provides(
-            self.graph_key,
-            GraphSpec(roi=Roi((-100, -100, -100), (200, 200, 200))),
-        )
-
-        self.provides(
-            self.array_key,
-            ArraySpec(
-                roi=Roi((-100, -100, -100), (200, 200, 200)),
-                voxel_size=Coordinate((4, 1, 1)),
-                interpolatable=False,
-            ),
-        )
-
-        self.provides(
-            self.array_key2,
-            ArraySpec(
-                roi=Roi((-100, -100, -100), (200, 200, 200)),
-                voxel_size=Coordinate((1, 2, 1)),
-                interpolatable=False,
-            ),
-        )
-
-    def node_to_voxel(self, array_roi, voxel_size, location):
-        # location is in world units, get it into voxels
+@pytest.fixture
+def mock_3d_source():
+    def node_to_voxel(array_roi, voxel_size, location):
         location = location / voxel_size
-
-        # shift location relative to beginning of array roi
         location -= array_roi.begin / voxel_size
-
         return tuple(slice(int(l - 1), int(l + 2)) for l in location)
 
-    def provide(self, request):
-        batch = Batch()
+    nodes = [
+        Node(id=1, location=np.array([0, 0.5, 0])),
+        Node(id=2, location=np.array([0, 10.5, 0])),
+        Node(id=3, location=np.array([0, 20.5, 0])),
+        Node(id=4, location=np.array([0, 30.5, 0])),
+        Node(id=5, location=np.array([0, 40.5, 0])),
+        Node(id=6, location=np.array([0, 50.5, 0])),
+    ]
+    g = Graph(
+        nodes, edges=[], spec=GraphSpec(roi=Roi((-100, -100, -100), (200, 200, 200)))
+    )
 
-        roi_graph = request[self.graph_key].roi
+    array_spec1 = ArraySpec(
+        roi=Roi((-100, -100, -100), (200, 200, 200)),
+        voxel_size=Coordinate((4, 1, 1)),
+        interpolatable=False,
+    )
+    data1 = da.zeros((array_spec1.roi.shape // array_spec1.voxel_size), dtype=np.uint32)
+    array_spec2 = ArraySpec(
+        roi=Roi((-100, -100, -100), (200, 200, 200)),
+        voxel_size=Coordinate((1, 2, 1)),
+        interpolatable=False,
+    )
+    data2 = da.zeros((array_spec2.roi.shape // array_spec2.voxel_size), dtype=np.uint32)
+    for node in nodes:
+        loc = node_to_voxel(array_spec1.roi, array_spec1.voxel_size, node.location)
+        data1[loc] = node.id
+        loc = node_to_voxel(array_spec2.roi, array_spec2.voxel_size, node.location)
+        data2[loc] = node.id
 
-        data = np.zeros(
-            (request[self.array_key].roi // self.spec[self.array_key].voxel_size).shape,
-            dtype=np.uint32,
-        )
+    array_1 = Array(
+        data1, offset=array_spec1.roi.offset, voxel_size=array_spec1.voxel_size
+    )
+    array_2 = Array(
+        data2, offset=array_spec2.roi.offset, voxel_size=array_spec2.voxel_size
+    )
 
-        for node in self.nodes:
-            loc = self.node_to_voxel(
-                request[self.array_key].roi,
-                self.spec[self.array_key].voxel_size,
-                node.location,
-            )
-            data[loc] = node.id
-
-        data2 = np.zeros(
-            (
-                request[self.array_key2].roi // self.spec[self.array_key2].voxel_size
-            ).shape,
-            dtype=np.uint32,
-        )
-
-        for node in self.nodes:
-            loc = self.node_to_voxel(
-                request[self.array_key2].roi,
-                self.spec[self.array_key2].voxel_size,
-                node.location,
-            )
-            data2[loc] = node.id
-
-        spec = self.spec[self.array_key].copy()
-        spec.roi = request[self.array_key].roi
-        batch.arrays[self.array_key] = Array(data, spec=spec)
-
-        spec2 = self.spec[self.array_key2].copy()
-        spec2.roi = request[self.array_key2].roi
-        batch.arrays[self.array_key2] = Array(data2, spec=spec2)
-
-        nodes = []
-        for node in self.nodes:
-            if roi_graph.contains(node.location):
-                nodes.append(node)
-        batch.graphs[self.graph_key] = Graph(
-            nodes=nodes, edges=[], spec=GraphSpec(roi=roi_graph)
-        )
-
-        return batch
+    return (
+        GPGraphSource(GraphKey("G"), g),
+        ArraySource(ArrayKey("A1"), array_1),
+        ArraySource(ArrayKey("A2"), array_2),
+    ) + MergeProvider()
 
 
 @pytest.mark.parametrize("rotate", [True, False])
 @pytest.mark.parametrize("spatial_dims", [2, 3])
+@pytest.mark.parametrize("rotation_axes", [None, (1, 0), (0, 2)])
 @pytest.mark.parametrize("fast_points", [True, False])
 @pytest.mark.parametrize("subsampling", [1, 2, 4])
-def test_3d_basics(rotate, spatial_dims, fast_points, subsampling):
-    test_labels = ArrayKey("TEST_LABELS")
-    test_labels2 = ArrayKey("TEST_LABELS2")
-    test_graph = GraphKey("TEST_GRAPH")
+def test_3d_basics(
+    mock_3d_source, rotate, spatial_dims, rotation_axes, fast_points, subsampling
+):
+    test_labels = ArrayKey("A1")
+    test_labels2 = ArrayKey("A2")
+    test_graph = GraphKey("G")
 
-    pipeline = GraphTestSource3D(test_graph, test_labels, test_labels2) + DeformAugment(
+    if spatial_dims <= max(rotation_axes or [0]):
+        pytest.skip("Rotation axes must be less than spatial dimensions")
+    pipeline = mock_3d_source + DeformAugment(
         [4] * spatial_dims,
         [1] * spatial_dims,
         graph_raster_voxel_size=[1] * spatial_dims,
@@ -138,6 +97,7 @@ def test_3d_basics(rotate, spatial_dims, fast_points, subsampling):
         spatial_dims=spatial_dims,
         use_fast_points_transform=fast_points,
         subsample=subsampling,
+        rotation_axes=rotation_axes,
     )
 
     for _ in range(5):
